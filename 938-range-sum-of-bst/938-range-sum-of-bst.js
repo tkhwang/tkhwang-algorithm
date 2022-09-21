6ddf3bb280af39a4e32f6817fc55df91637d3bb7
tkhwang <tkhwang@gmail.com>
2022-09-21 19:09:15 +0900

    Time: 315 ms (10.85%), Space: 96.7 MB (37.34%) - LeetHub

---
+++ 938-range-sum-of-bst/938-range-sum-of-bst.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function(root, low, high) {
+    let sum = 0;
+    
+    const dfs = (node) => {
+        if (!node) return;
+        
+        if (low <= node.val && node.val <= high) sum += node.val;
+        
+        dfs(node.left);
+        dfs(node.right);
+    }
+    
+    dfs(root);
+    
+    return sum;
+};
