c28ca12008c67b4f31fa0ce26ee770392e2e4808
tkhwang <tkhwang@gmail.com>
2022-09-24 15:38:45 +0900

    Time: 150 ms (47.80%), Space: 47.7 MB (75.17%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number}
+ */
+var pathSum = function(root, targetSum) {
+    const map = {};
+    let count = 0;
+    
+    const dfs = (node, sum) => {
+        if (!node) return null;
+        
+        sum += node.val;
+        
+        if (sum === targetSum) count += 1;
+        if (map[sum - targetSum] !== undefined) count += map[sum - targetSum];
+
+        map[sum] = (map[sum] || 0) +1;
+
+        dfs(node.left, sum);
+        dfs(node.right, sum);
+
+        map[sum] -= 1;
+    }
+    
+    dfs(root, 0);
+    
+    return count;
+};
