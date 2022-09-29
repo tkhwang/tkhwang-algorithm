5704dbff575c5dce217ad3138bee170d9d44fda9
tkhwang <tkhwang@gmail.com>
2022-09-29 10:12:57 +0900

    Time: 275 ms (5.03%), Space: 51 MB (9.46%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.js
+++ 658-find-k-closest-elements/658-find-k-closest-elements.js
@@ -5,7 +5,17 @@
  * @return {number[]}
  */
 var findClosestElements = function(arr, k, x) {
-    return arr.sort((a,b) => Math.abs(a - x) - Math.abs(b-x))
-            .slice(0, k)
-            .sort((a,b) => a - b)
+    const minHeap = new MinPriorityQueue({ compare: (a,b) => Math.abs(a-x) - Math.abs(b-x) || a - b });
+    
+    for (const n of arr) {
+        minHeap.enqueue(n);
+    }
+    
+    const result = [];
+    while (k > 0) {
+        result.push(minHeap.dequeue());
+        k -= 1;
+    }
+    
+    return result.sort((a,b) => a - b)
 };
