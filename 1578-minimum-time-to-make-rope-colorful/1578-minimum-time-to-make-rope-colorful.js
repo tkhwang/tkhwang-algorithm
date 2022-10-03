ea3619d26dd7a9fa436a4aa1aaf3cd3a39f2a620
tkhwang <tkhwang@gmail.com>
2022-10-03 12:56:12 +0900

    Time: 2420 ms (5.52%), Space: 63.7 MB (11.72%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} colors
+ * @param {number[]} neededTime
+ * @return {number}
+ */
+var minCost = function(colors, neededTime) {
+    const N = colors.length;
+    let prvIndex = 0;
+    let res = 0;
+    
+    for (let i = 1; i < N; i += 1) {
+        console.log(i, prvIndex);
+        
+        const cur = colors[i];
+        
+        if (colors[prvIndex] !== cur)  {
+            prvIndex = i;
+            continue;
+        } 
+        
+        if (neededTime[prvIndex] > neededTime[i]) {
+            res += neededTime[i];
+        } else {
+            res += neededTime[prvIndex];
+            prvIndex = i;
+        }
+    }
+    
+    return res;
+};
