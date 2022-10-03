990f82419b0ec56c045c655dc474ecdb6d5b8bce
tkhwang <tkhwang@gmail.com>
2022-10-03 22:07:01 +0900

    Time: 154 ms (75.09%), Space: 50.7 MB (45.17%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.js
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.js
@@ -4,35 +4,35 @@
  */
 var longestIncreasingPath = function(matrix) {
     const [ ROWS, COLS ] = [ matrix.length, matrix[0].length ];
-
-    const directions = [ [1,0], [-1, 0], [0, 1], [0, -1] ];
+    
+    let max = -Infinity;
+    const cache = new Map();
+    
+    const directions = [ [1,0], [-1,0], [0,1], [0,-1] ];
     const isValid = (r, c) => !(r < 0 || r >= ROWS || c < 0 || c >= COLS);
     const genKey = (r, c) => `${r}:${c}`;
     
-    const cache = Array(ROWS).fill(null).map((_) => Array(COLS).fill(-1));
-    let max = -Infinity;
-
     const dfs = (r, c, parent) => {
-        if (!isValid(r,c)) return 0;
+        if (!isValid(r, c)) return 0;
         if (parent >= matrix[r][c]) return 0;
+        if (cache.has(genKey(r, c))) return cache.get(genKey(r, c));
         
-        if (cache[r][c] !== -1) return cache[r][c];
+        let localMax = -Infinity;
         
-        let maxPath = 0;
         for (const [ dR, dC ] of directions) {
             const newR = r + dR;
             const newC = c + dC;
             
-            const maxForNode = 1 + dfs(newR, newC, matrix[r][c]);
-            maxPath = Math.max(maxPath, maxForNode);
+            localMax = Math.max(localMax, 1 + dfs(newR, newC, matrix[r][c]));
         }
-        cache[r][c] = maxPath;
-        return cache[r][c];
+        
+        cache.set(genKey(r, c), localMax);
+        return localMax;
     }
     
     for (let r = 0; r < ROWS; r += 1) {
-        for (let c = 0; c < COLS; c += 1) {
-            max = Math.max(max, dfs(r, c, -Infinity))
+        for (let c = 0 ; c < COLS; c += 1) {
+            max = Math.max(max, dfs(r, c, -Infinity));
         }
     }
     
