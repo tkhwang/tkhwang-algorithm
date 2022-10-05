b69eda00d718f6b29b2bb3346a397a48b7ef9ec9
tkhwang <tkhwang@gmail.com>
2022-10-05 12:13:55 +0900

    Time: 104 ms (89.66%), Space: 47.9 MB (72.41%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.js
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.js
@@ -14,17 +14,22 @@
  */
 var addOneRow = function(root, val, depth) {
     
-    const dfs = (node, val,depth) => {
-        if (depth === 1) return new TreeNode(val, node, null);
-        if (depth === 2) {
+    const dfs = (node, depth) => {
+        if (!node) return node;
+        
+        if (depth === 1) {
             node.left = new TreeNode(val, node.left, null);
             node.right = new TreeNode(val, null, node.right);
-        } else {
-            if (node.left) dfs(node.left, val, depth - 1);
-            if (node.right) dfs(node.right, val, depth - 1);
+            return node;
         }
+        
+        node.left = dfs(node.left, depth - 1);
+        node.right = dfs(node.right, depth - 1);
         return node;
     }
     
-    return dfs(root, val, depth);
+    const newRoot = new TreeNode(-1, root);
+    dfs(newRoot, depth)
+    
+    return newRoot.left;
 };
