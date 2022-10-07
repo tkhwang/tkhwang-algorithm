340e7c709ebf873bc7c8fb396ed8996ca16ab1a4
tkhwang <tkhwang@gmail.com>
2022-10-07 18:12:52 +0900

    Time: 393 ms (5.11%), Space: 52.5 MB (5.07%) - LeetHub

---
+++ 139-word-break/139-word-break.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+    const trie = {};
+    for (const word of wordDict) {
+        let node = trie;
+        for (const ch of word) {
+            if (node[ch] === undefined) node[ch] = {};
+            node = node[ch];
+        }
+        node.isWord = true;
+        node.wordLength = word.length;
+    }
+    
+    const N = s.length;
+    
+    const dp = Array(N + 1).fill(false);
+    dp[0] = true;
+    
+    const search = (index, parent = trie) => {
+        let node = parent;
+        for (let i = index; i < N; i += 1) {
+            const cur = s[i];
+            
+            console.log(i, cur, node)
+            
+            if (node[cur] === undefined) return;
+            
+            if (node[cur].isWord) {
+                dp[i+1] = true;
+            }
+            
+            node = node[cur];
+        }
+    }
+    
+    for (let i = 0; i < N + 1; i += 1) {
+        if (!dp[i]) continue;
+        
+        search(i)
+    }
+    
+    console.log(dp);
+    
+    return dp[N];
+};
