f275592b7d5db24f7971226277feeee597ca7987
Taekeun Hwang <tkhwang@gmail.com>
2022-10-10 00:58:08 +0900

    feat: :recycle: re-arrange by difficulty

--- _2_medium/53-maximum-subarray/53-maximum-subarray.js
+++ _2_medium/53-maximum-subarray/53-maximum-subarray.js
@@ -6,12 +6,11 @@ var maxSubArray = function(nums) {
     const N = nums.length;
     
     const dp = Array(N).fill(0);
-
     dp[0] = nums[0];
-    
+
     for (let i = 1; i < N; i += 1) {
         dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
     }
-
+    
     return Math.max(...dp);
 };
