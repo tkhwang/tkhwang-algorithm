7025a10956d2ca3754a268d319dce789ed4637e5
tkhwang <tkhwang@gmail.com>
2022-10-10 13:42:22 +0900

    Time: 94 ms (93.72%), Space: 44.5 MB (18.89%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
@@ -5,14 +5,36 @@
 var lengthOfLIS = function(nums) {
     const N = nums.length;
     
-    const dp = Array(N).fill(1);
-    
-    for (let i = 1; i < N; i += 1) {
-        for (let j = 0; j < i; j += 1) {
-            if (nums[i] > nums[j]) {
-                dp[i] = Math.max(dp[i], 1 + dp[j]);
+    const bisectLeft = (array, target) => {
+        const N = array.length;
+        
+        // [0, N)
+        let left = 0;
+        let right = N;
+        
+        while (left < right) {
+            const mid = Math.floor((left + right)/2);
+            
+            // => [left, mid)
+            if (array[mid] === target) {
+                right = mid;
+            // select left space => [left, mid)
+            } else if (array[mid] > target) {
+                right = mid;
+            // select right space => [mid + 1, right)
+            } else if (array[mid] < target) {
+                left = mid + 1;
             }
         }
+        return left;
     }
-    return Math.max(...dp);
+    
+    const sub = [];
+    for (const num of nums) {
+        const index = bisectLeft(sub, num);
+        if (index === sub.length) sub.push(num)
+        else sub[index] = num;
+    }
+    
+    return sub.length;
 };
