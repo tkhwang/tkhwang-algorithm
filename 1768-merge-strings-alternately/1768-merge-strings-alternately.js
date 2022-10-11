6e60202c1664f3b2627488ae3145b32cd72bbf9c
tkhwang <tkhwang@gmail.com>
2022-10-11 22:58:59 +0900

    Time: 77 ms (80.34%), Space: 42.2 MB (66.10%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    const N1 = word1.length;
+    const N2 = word2.length;
+    
+    let res = "";
+    
+    let i = 0;
+    let j = 0;
+    
+    while (i < N1 && j < N2) {
+        
+        res += word1[i];
+        res += word2[j];
+        
+        i += 1;
+        j += 1;
+    }
+    
+    while (i < N1) {
+        res += word1[i];
+        i += 1;
+    }
+    
+    while (j < N2) {
+        res += word2[j];
+        j += 1;
+    }
+    
+    return res;
+};
