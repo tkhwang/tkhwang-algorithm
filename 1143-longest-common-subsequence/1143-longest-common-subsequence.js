debd5d42b1545e6d91591b8beb5ff4790caecf7f
tkhwang <tkhwang@gmail.com>
2022-10-11 12:01:53 +0900

    Time: 1504 ms (7.48%), Space: 168.3 MB (6.70%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -7,16 +7,20 @@ var longestCommonSubsequence = function(text1, text2) {
     const N1 = text1.length;
     const N2 = text2.length;
     
-    const dp = Array(N1+1).fill(null).map((_) => Array(N2+1).fill(0));
+    const cache = {};
+    const genKey = (a, b) => `${a}:${b}`;
     
-    for (let r = 1; r <= N1; r += 1) {
-        for (let c = 1; c <= N2; c += 1) {
-            if (text1[r-1] === text2[c-1]) {
-                dp[r][c] = 1 + dp[r-1][c-1];
-            } else {
-                dp[r][c] = Math.max(dp[r-1][c], dp[r][c-1]);
-            }
+    const dp = (len1, len2) => {
+        if (len1 < 0 || len2 < 0) return 0;
+        if (cache[genKey(len1, len2)] !== undefined) return cache[genKey(len1, len2)];
+        
+        if (text1[len1] === text2[len2]) {
+            cache[genKey(len1, len2)] =  1 + dp(len1 -1, len2 -1);
+        } else {
+            cache[genKey(len1, len2)] = Math.max(dp(len1-1, len2), dp(len1, len2-1));
         }
+        return cache[genKey(len1, len2)];
     }
-    return dp[N1][N2]
+    
+    return dp(N1-1, N2-1)
 };
