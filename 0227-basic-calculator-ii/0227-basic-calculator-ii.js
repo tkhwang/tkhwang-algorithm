1501a1a98f2f7afc3b3bdd5daa79f7f4063fcc61
tkhwang <tkhwang@gmail.com>
2022-10-13 17:05:47 +0900

    Time: 155 ms (35.23%), Space: 54.6 MB (15.14%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(_s) {
+    const s = _s.replace(/\s/g, "").split("");
+    const N = s.length;
+    
+    const stack = [];
+    let n = 0;
+    let op = "+";
+    
+    for (const [i, c] of s.entries()) {
+        
+        if (!isNaN(c)) {
+            n = 10 * n + (c - '0');
+        }
+        
+        if (isNaN(c) || i === N - 1) {
+            if (op === "+") {
+                stack.push(+n * 1);
+            } else if (op === "-") {
+                stack.push(+n * -1);
+            } else if (op === "*") {
+                const pre = stack.pop();
+                stack.push(pre * n);
+            } else if (op === "/") {
+                const pre = stack.pop();
+                stack.push(parseInt(pre / n, 10 ));
+            }
+            op = c;
+            n = 0;
+        } 
+    }
+    
+    return stack.reduce((a,b) => a + b, 0);
+};
