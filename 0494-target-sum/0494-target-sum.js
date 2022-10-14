d67cde0849c834ed3e4f44c96f1948742ab7e5d7
tkhwang <tkhwang@gmail.com>
2022-10-15 00:23:45 +0900

    Time: 5860 ms (11.15%), Space: 42 MB (84.80%) - LeetHub

--- 0494-target-sum/0494-target-sum.js
+++ 0494-target-sum/0494-target-sum.js
@@ -5,19 +5,32 @@
  */
 var findTargetSumWays = function(nums, target) {
     const N = nums.length;
-    let count = 0;
-    
-    const backtrack = (i, sum) => {
-        if (i >= N) {
-            if (sum === target) count += 1;
-            return;
+
+    const dp = (index, sum) => {
+        if (index >= N) {
+            return sum === 0 ? 1 : 0;
         }
         
-        backtrack(i + 1, sum + nums[i]);
-        backtrack(i + 1, sum - nums[i]);
+        return dp(index + 1, sum + nums[index]) + dp(index + 1, sum - nums[index]);
     }
-    
-    backtrack(0, 0);
-    
-    return count;
+
+
+    return dp(0, target)
 };
+
+//     const N = nums.length;
+//     let count = 0;
+    
+//     const backtrack = (i, sum) => {
+//         if (i >= N) {
+//             if (sum === target) count += 1;
+//             return;
+//         }
+        
+//         backtrack(i + 1, sum + nums[i]);
+//         backtrack(i + 1, sum - nums[i]);
+//     }
+    
+//     backtrack(0, 0);
+    
+//     return count;
