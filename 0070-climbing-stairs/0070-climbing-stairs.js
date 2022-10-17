e1d8bd79dc666b3ac1305a8bcd00dabff5a60b09
tkhwang <tkhwang@gmail.com>
2022-10-17 18:08:30 +0900

    Time: 95 ms (43.94%), Space: 42.5 MB (6.88%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    const cache = {};
+    
+    const dp = (n) => {
+        if (n === 1) return 1;
+        if (n === 2) return 2;
+        if (cache[n]) return cache[n];
+        
+        return cache[n] = dp(n-1) + dp(n-2)
+    }
+    
+    return dp(n)
+};
