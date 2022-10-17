a24ffae74e09237d0d956a06a0d8d1fa4c95e303
tkhwang <tkhwang@gmail.com>
2022-10-17 10:17:44 +0900

    Time: 571 ms (100.00%), Space: 92.1 MB (100.00%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.js
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.js
@@ -4,29 +4,15 @@
  * @return {number[]}
  */
 var productQueries = function(n, queries) {
-    
+    const MOD = 7 + 10 ** 9;    
     
     const powers = [ ];
-    const MOD = 7 + 10 ** 9;
     for (let i = 0; i < 32; i += 1) {
         if (n & (1 << i)) {
             powers.push(1<<i);
         }
     }
     const N = powers.length;
-
-//     const prefixProduct = Array(N).fill(0);
-
-//     let product = 1;
-//     for(let i = 0; i < N; i += 1) {
-//         product *= powers[i];
-//         prefixProduct[i] = product;
-//     }
-
-//     const result = [];
-//     for (const [ start , end ] of queries) {
-//         result.push((prefixProduct[end] / prefixProduct[start] * powers[start]) % MOD)
-//     }
     
     const res = [];
     
