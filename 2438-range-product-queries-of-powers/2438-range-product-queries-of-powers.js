51cb18f9cccd1bfb0b145503c06460142ab19e2c
tkhwang <tkhwang@gmail.com>
2022-10-17 10:17:03 +0900

    Time: 375 ms (100.00%), Space: 81.4 MB (100.00%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.js
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.js
@@ -5,31 +5,38 @@
  */
 var productQueries = function(n, queries) {
     
+    
     const powers = [ ];
     const MOD = 7 + 10 ** 9;
-    
-
     for (let i = 0; i < 32; i += 1) {
         if (n & (1 << i)) {
             powers.push(1<<i);
         }
     }
-
-    const isPowerOfTwo = (n) => (Math.log(n)/Math.log(2)) % 1 === 0
-    
     const N = powers.length;
-    const prefixProduct = Array(N).fill(0);
 
-    let product = 1;
-    for(let i = 0; i < N; i += 1) {
-        product *= powers[i];
-        prefixProduct[i] = product;
-    }
+//     const prefixProduct = Array(N).fill(0);
 
-    const result = [];
-    for (const [ start , end ] of queries) {
-        result.push((prefixProduct[end] / prefixProduct[start] * powers[start]) % MOD)
+//     let product = 1;
+//     for(let i = 0; i < N; i += 1) {
+//         product *= powers[i];
+//         prefixProduct[i] = product;
+//     }
+
+//     const result = [];
+//     for (const [ start , end ] of queries) {
+//         result.push((prefixProduct[end] / prefixProduct[start] * powers[start]) % MOD)
+//     }
+    
+    const res = [];
+    
+    for (const [ start, end ] of queries) {
+        let product = 1;
+        for (let i = start; i <= end; i += 1) {
+            product = (product * powers[i]) % MOD;
+        }
+        res.push(product);
     }
     
-    return result;
+    return res;
 };
