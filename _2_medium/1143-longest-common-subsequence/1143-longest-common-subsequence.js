301663dbc0d7fcc12d6fd8cd48231142dcf73aa0
tkhwang <tkhwang@gmail.com>
2022-10-19 08:39:20 +0900

    feat: :recycle: re-arrange by difficulty

--- _2_medium/1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ _2_medium/1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -7,20 +7,37 @@ var longestCommonSubsequence = function(text1, text2) {
     const N1 = text1.length;
     const N2 = text2.length;
     
-    const cache = {};
-    const genKey = (a, b) => `${a}:${b}`;
+    const dp = Array(N1 + 1).fill(null).map((_) => Array(N2 + 1).fill(0));
     
-    const dp = (len1, len2) => {
-        if (len1 < 0 || len2 < 0) return 0;
-        if (cache[genKey(len1, len2)] !== undefined) return cache[genKey(len1, len2)];
-        
-        if (text1[len1] === text2[len2]) {
-            cache[genKey(len1, len2)] =  1 + dp(len1 -1, len2 -1);
-        } else {
-            cache[genKey(len1, len2)] = Math.max(dp(len1-1, len2), dp(len1, len2-1));
+    for (let i = 1; i <= N1; i += 1) {
+        for (let j = 1; j <= N2; j += 1) {
+            if (text1[i-1] === text2[j-1]) {
+                dp[i][j] = 1 + dp[i-1][j-1];
+            } else {
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
         }
-        return cache[genKey(len1, len2)];
     }
-    
-    return dp(N1-1, N2-1)
+    return dp[N1][N2];
 };
+
+//     const cache = {};
+//     const genKey = (i, j) => `${i}:${j}`;
+    
+//     // dp(i, j) : text1[i], text2[j] 지점의 LCS 갯수
+    
+//     const dp = (i, j) => {
+//         const key = genKey(i, j)
+        
+//         if (i === 0 || j === 0) return 0;
+//         if (cache[key] !== undefined) return cache[key];
+        
+//         if (text1[i-1] === text2[j-1]) {
+//             cache[key] =  1 + dp(i-1, j-1);
+//         } else {
+//             cache[key] = Math.max(dp(i-1, j), dp(i, j-1));
+//         }
+//         return cache[key];
+//     }
+    
+//     return dp(N1, N2);
--- _3_hard/0072-edit-distance/0072-edit-distance.js
+++ _3_hard/0072-edit-distance/0072-edit-distance.js
@@ -8,24 +8,26 @@ var minDistance = function(word1, word2) {
     const N2 = word2.length;
     
     const cache = {};
-    const genKey = (a, b) => `${a}:${b}`;
+    const genKey = (i, j) => `${i}:${j}`;
     
-    const dp = (len1, len2) => {
-        if (len1 < 0) return len2 + 1;
-        if (len2 < 0) return len1 + 1;
-        if (cache[genKey(len1, len2)] !== undefined) return cache[genKey(len1, len2)];
+    const dp = (i, j) => {
+        const key = genKey(i, j);
         
-        if (word1[len1] === word2[len2]) {
-            cache[genKey(len1, len2)] = dp(len1 -1, len2 - 1);
+        if (i < 1) return j;
+        if (j < 1) return i;
+        if (cache[key] !== undefined) return cache[key];
+        
+        if (word1[i-1] === word2[j-1]) {
+            cache[key] = dp(i - 1, j- 1);
         } else {
-            cache[genKey(len1, len2)] = Math.min(
-                1 + dp(len1 - 1, len2),
-                1 + dp(len1, len2 - 1),
-                1 + dp(len1-1, len2 - 1)
+            cache[key] = Math.min(
+                1 + dp(i-1, j),
+                1 + dp(i, j-1),
+                1 + dp(i-1, j-1)
             )
         }
-        return cache[genKey(len1, len2)];
+        return cache[key];
     }
     
-    return dp(N1-1, N2-1)
+    return dp(N1, N2);
 };
