02044e016c7f1a00cb8fcec41ce4a83bd11b8c7a
tkhwang <tkhwang@gmail.com>
2022-10-20 12:04:31 +0900

    Time: 220 ms (53.44%), Space: 48 MB (52.83%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.js
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -3,14 +3,13 @@
  * @return {string}
  */
 var intToRoman = function(num) {
-
     const M2Roman = [ "", "M", "MM", "MMM" ];
     const C2Roman = [ "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" ];
     const X2Roman = [ "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" ];
     const I2Roman = [ "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" ];
     
-    return "" + M2Roman[Math.floor(num/1000)]
-              + C2Roman[Math.floor((num%1000)/100)]
-              + X2Roman[Math.floor((num%100)/10)]
-              + I2Roman[num%10]
+    return M2Roman[Math.floor(num/1000)]
+           + C2Roman[Math.floor((num%1000)/100)]
+           + X2Roman[Math.floor((num%100)/10)]
+           + I2Roman[num%10]
 };
