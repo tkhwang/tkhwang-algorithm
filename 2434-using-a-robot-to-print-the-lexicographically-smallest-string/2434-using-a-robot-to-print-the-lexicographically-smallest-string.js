713e7ee6cd95ee41cd7fee41c08d2e5c73f40162
tkhwang <tkhwang@gmail.com>
2022-10-27 15:47:33 +0900

    Time: 490 ms (57.64%), Space: 96.9 MB (18.75%) - LeetHub

---
+++ 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var robotWithString = function(_s) {
+    const N = _s.length;
+    const s = _s.split("");
+
+    const suffixMin = Array(N).fill(0);
+    suffixMin[N-1] = s[N-1]
+    for (let i = N-2; i >= 0; i -= 1) {
+        const latest = suffixMin[i+1];
+        const cur = s[i];
+        
+        suffixMin[i] = cur < latest ? cur : suffixMin[i+1];
+    }
+
+    console.log(suffixMin);
+    
+    const t = [];
+    let res = "";
+
+    for (const [ i, ch ] of s.entries()) {
+        while (t.length && t[t.length - 1] <= suffixMin[i]) res += t.pop();
+        t.push(ch);
+    }
+    
+    while (t.length) {
+        res += t.pop();
+    }
+    
+    return res;
+};
