036b64c5007af9ee592a51da420b40a93fefe987
tkhwang <tkhwang@gmail.com>
2022-10-28 00:48:14 +0900

    Time: 184 ms (52.44%), Space: 47.3 MB (48.51%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    const N = s.length;
+    let max = -Infinity;
+    let maxValue;
+    
+    for (let i = 0; i < N; i += 1) {
+        let [ left, right ] = [ i, i ];
+        while (0 <= left && right < N && s[left] === s[right]) {
+            if (max < right - left + 1) {
+                max = right - left + 1;
+                maxValue = s.slice(left, right + 1);
+            }        
+            left -= 1;
+            right += 1;
+        }
+        
+        [ left, right ] = [ i, i + 1 ];
+        while (0 <= left && right < N && s[left] === s[right]) {
+            if (max < right - left + 1) {
+                max = right - left + 1;
+                maxValue = s.slice(left, right + 1);
+            }
+            left -= 1;
+            right += 1;
+        }
+    }
+    
+    return maxValue;
+};
