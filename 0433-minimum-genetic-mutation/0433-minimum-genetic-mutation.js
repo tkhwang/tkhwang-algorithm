43145cfce1382fde7e65769f0ea58047dcd8319c
tkhwang <tkhwang@gmail.com>
2022-11-02 10:15:43 +0900

    Time: 99 ms (37.29%), Space: 42.6 MB (6.78%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} start
+ * @param {string} end
+ * @param {string[]} bank
+ * @return {number}
+ */
+var minMutation = function(start, end, bank) {
+    const genes = [ "A", "C", "G", "T" ];
+    
+    const queue = [ start ];
+    const set = new Set(bank);
+    const seen = new Set();
+    // seen.add(start);
+    
+    let steps = 0;
+    
+    while (queue.length) {
+        const len = queue.length;
+        
+        for (let i = 0; i < len; i += 1) {
+            const cur = queue.shift();
+            
+            if (cur === end) return steps;
+
+            for (let j = 0; j < cur.length; j += 1) {
+                // if (cur[j] === end[j]) continue;
+                
+                for (const gene of genes) {
+                    const next = cur.substring(0, j) + gene + cur.substring(j+1);
+                    
+                    // console.log(cur, next);
+                    
+                    if (set.has(next) && !seen.has(next)) {
+                        seen.add(next);
+                        queue.push(next);
+                    }
+                }
+            }
+        }
+        
+        steps += 1;
+    }
+    
+    return -1;
+};
