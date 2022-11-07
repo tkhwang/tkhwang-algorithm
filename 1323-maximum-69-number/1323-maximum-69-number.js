8ae3c7cd887fa26041a81d87805d5c821a4961a3
tkhwang <tkhwang@gmail.com>
2022-11-07 09:03:44 +0900

    Time: 102 ms (36.12%), Space: 41.4 MB (97.97%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number  = function(_num) {
+    const num = String(_num).split("");
+    
+    const N = num.length;
+    for (let i = 0; i < N; i += 1) {
+        if (num[i] === '6') {
+            num[i] = '9'
+            break;
+        }
+    }
+    
+    return num.join("");
+};
