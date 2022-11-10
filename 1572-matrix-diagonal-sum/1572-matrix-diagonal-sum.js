7b36fa45d051fd99729df84b9e1c0d8f7f2da182
tkhwang <tkhwang@gmail.com>
2022-11-10 09:58:20 +0900

    Time: 102 ms (47.12%), Space: 44.8 MB (5.91%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function(mat) {
+    const [ ROWS, COLS ] = [ mat.length, mat[0].length ];
+    
+    let sum = 0;
+    
+    for (let r = 0; r < ROWS; r += 1) {
+        for (let c = 0; c < COLS; c += 1) {
+            if (r === c) sum += mat[r][c];
+            if (r + c === ROWS - 1) sum += mat[r][c];
+        }
+    }
+    
+    if (ROWS % 2 === 1) {
+        const mid = Math.floor(ROWS/2);
+        sum -= mat[mid][mid];
+    }
+    
+    return sum;
+};
