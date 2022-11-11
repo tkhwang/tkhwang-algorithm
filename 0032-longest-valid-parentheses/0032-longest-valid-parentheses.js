e0e231d4e486d53d1de66ccfbe13221c84d95fbe
tkhwang <tkhwang@gmail.com>
2022-11-11 15:15:00 +0900

    Time: 91 ms (79.18%), Space: 47.1 MB (9.24%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.js
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.js
@@ -4,18 +4,17 @@
  */
 var longestValidParentheses = function(s) {
     const N = s.length;
+    
     const stack = [ -1 ];
     let max = 0;
     
     for (const [ i, ch ] of s.split("").entries()) {
         if (ch === "(") {
-            stack.push(i)        
+            stack.push(i)
         } else {
             stack.pop();
             if (stack.length === 0) stack.push(i);
-            else {
-                max = Math.max(max, i - stack.at(-1))
-            }
+            else max = Math.max(max, i - stack.at(-1));
         }
     }
     
