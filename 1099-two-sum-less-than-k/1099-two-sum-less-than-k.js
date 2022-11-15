e7385cfab0b7e7f08abbffe0eeaba50565b6f9df
tkhwang <tkhwang@gmail.com>
2022-11-15 09:52:45 +0900

    Time: 113 ms (29.56%), Space: 42.6 MB (36.95%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.js
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.js
@@ -8,14 +8,18 @@ var twoSumLessThanK = function(nums, k) {
     
     nums.sort((a,b) => a - b);
 
+    let left = 0;
+    let right = N - 1;
     let max = -Infinity;
     
-    for (let i = 0; i < N; i += 1) {
-        for (let j = i +1; j < N; j += 1) {
-            const sum = nums[i] + nums[j];
-            if (sum < k) {
-                if (max < sum) max = sum;
-            }
+    while (left < right) {
+        const sum = nums[left] + nums[right];
+        
+        if (sum < k) {
+            if (max < sum) max = sum;
+            left += 1;
+        } else {
+            right -= 1;
         }
     }
     return max === -Infinity ? -1 : max;
