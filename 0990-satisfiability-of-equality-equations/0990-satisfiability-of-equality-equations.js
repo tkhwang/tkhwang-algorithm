a59b11e9cee832b3052c80a71bc7528159529687
tkhwang <tkhwang@gmail.com>
2022-11-17 12:33:24 +0900

    Time: 145 ms (34.44%), Space: 47.7 MB (20.34%) - LeetHub

---
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.js
@@ -0,0 +1,65 @@
+/**
+ * @param {string[]} equations
+ * @return {boolean}
+ */
+var equationsPossible = function(equations) {
+    const N = equations.length;
+    
+    const unionFind = new UnionFind(26);
+    
+    const getCode = (ch) => ch.charCodeAt(0) - 'a'.charCodeAt(0);
+    
+    for (const equation of equations) {
+        const [ left, isEqual, _, right ] = equation.split("")
+        if (isEqual === "!") continue;
+        
+        unionFind.union(getCode(left), getCode(right));
+    }
+    
+    for (const equation of equations) {
+        const [ left, isEqual, _, right ] = equation.split("")
+        if (isEqual === "=") continue;
+        
+        if (unionFind.connected(getCode(left), getCode(right))) return false;
+    }    
+    return true;
+};
+
+class UnionFind {
+    constructor(n) {
+        this.root = Array(n).fill(null).map((_,i) => i);
+        this.rank = Array(n).fill(1);
+        this.components = n;
+    }
+    
+    find(node) {
+        if (node === this.root[node]) return node;
+        return this.root[node] = this.find(this.root[node]);
+    }
+    
+    union(x, y) {
+        const rootX = this.find(x);
+        const rootY = this.find(y);
+        
+        if (rootX === rootY) return false;
+        
+        if (this.rank[rootX] < this.rank[rootY]) {
+            this.root[rootX] = rootY;
+        } else if (this.rank[rootX] > this.rank[rootY]) {
+            this.root[rootY] = rootX;
+        } else {
+            this.root[rootY] = rootX;
+            this.rank[rootX] += 1;
+        }
+        this.components -= 1;
+        return true
+    }
+    
+    connected(x,y) {
+        return this.find(x) === this.find(y)
+    }
+    
+    getComponents() {
+        return this.components;
+    }
+}
