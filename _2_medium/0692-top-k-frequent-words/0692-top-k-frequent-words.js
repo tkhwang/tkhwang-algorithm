7b56065fd3a901a0775489fd4e0e28d9e7c8d9db
tkhwang <tkhwang@gmail.com>
2022-11-18 12:00:47 +0900

    feat: :recycle: rearrange by difficulty

--- _2_medium/0692-top-k-frequent-words/0692-top-k-frequent-words.js
+++ _2_medium/0692-top-k-frequent-words/0692-top-k-frequent-words.js
@@ -5,13 +5,11 @@
  */
 var topKFrequent = function(words, k) {
     const freq = {};
-    
     for (const word of words) {
         freq[word] = (freq[word] || 0) + 1;
     }
     
     const keys = Object.keys(freq);
-
     keys.sort((a,b) => freq[b] - freq[a] || a.localeCompare(b));
     
     return keys.slice(0, k);
