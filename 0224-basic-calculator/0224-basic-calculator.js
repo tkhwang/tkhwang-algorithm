f0ad27227536ddb5f06e7e978942f93b4734b058
tkhwang <tkhwang@gmail.com>
2022-11-21 00:21:32 +0900

    Time: 142 ms (49.29%), Space: 44.8 MB (66.29%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+    let sign = 1
+    let sum = 0;
+    const stack = []; 
+    
+    for (let i = 0; i < s.length; i += 1) {
+        if (s[i] >= '0' && s[i] <= '9') {
+            let num = 0
+            while (s[i] >= '0' && s[i] <= '9') {
+                num = (num * 10) + (s[i] - '0');
+                i += 1;
+            }
+            sum += (num * sign);
+            i -= 1;
+        } else if (s[i] === '+') {
+            sign = 1;
+        } else if (s[i] === '-') {
+            sign = -1;
+        } else if (s[i] === '(') {
+            stack.push(sum);
+            stack.push(sign);
+            sum = 0
+            sign = 1;
+        } else if (s[i] === ')') {
+            sum = stack.pop() * sum;
+            sum += stack.pop();
+        }
+    }
+    
+    return sum;
+};
