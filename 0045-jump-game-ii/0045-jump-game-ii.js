81039fd4dc99cf7c52fad66e80fafb496c404e28
tkhwang <tkhwang@gmail.com>
2022-11-23 17:21:07 +0900

    Time: 265 ms (24.54%), Space: 48.9 MB (9.06%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    const N = nums.length;
+    
+    const dp = Array(N).fill(Infinity);
+    dp[0] = 0;
+    
+    for (let i = 0; i < N; i += 1) {
+        const steps = nums[i];
+        for (let j = 1; j <= steps; j += 1) {
+            dp[i+j] = Math.min(
+                dp[i+j],
+                dp[i] + 1
+            )
+        }
+    }
+    
+    return dp[N-1];
+};
