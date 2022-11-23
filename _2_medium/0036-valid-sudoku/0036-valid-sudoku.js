12e8a727c30f4fe9be5040e66a09a68a7bf17c41
tkhwang <tkhwang@gmail.com>
2022-11-23 14:01:13 +0900

    re-arrange by difficulty

--- _2_medium/0036-valid-sudoku/0036-valid-sudoku.js
+++ _2_medium/0036-valid-sudoku/0036-valid-sudoku.js
@@ -4,25 +4,27 @@
  */
 var isValidSudoku = function(board) {
     const [ ROWS, COLS ] = [ board.length, board[0].length ];
-    const m = Math.floor(ROWS/3);
     
-    const rowSet = Array(ROWS).fill(null).map((_) => new Set());
-    const colSet = Array(COLS).fill(null).map((_) => new Set());
-    const diaSet = Array(ROWS).fill(null).map((_) => new Set());
+    const setRows = Array(ROWS).fill(null).map((_) => new Set());
+    const setCols = Array(COLS).fill(null).map((_) => new Set());
+    const setBoxs = Array(ROWS).fill(null).map((_) => new Set());
     
-    const rc2dia = (r, c) =>  Math.floor(r/m)*m + Math.floor(c/m);
+    const m = ROWS/3;
+    const rc2box = (r, c) => Math.floor(r/m)*m + Math.floor(c/m);
     
     for (let r = 0; r < ROWS; r += 1) {
         for (let c = 0; c < COLS; c += 1) {
             const cur = board[r][c];
             if (cur === ".") continue;
             
-            if (rowSet[r].has(cur)) return false;
-            rowSet[r].add(cur);
-            if (colSet[c].has(cur)) return false;
-            colSet[c].add(cur)
-            if (diaSet[rc2dia(r, c)].has(cur)) return false;
-            diaSet[rc2dia(r, c)].add(cur);
+            if (setRows[r].has(cur)) return false;
+            setRows[r].add(cur);
+            
+            if (setCols[c].has(cur)) return false;
+            setCols[c].add(cur);
+            
+            if (setBoxs[rc2box(r, c)].has(cur)) return false;
+            setBoxs[rc2box(r, c)].add(cur);
         }
     }
     
