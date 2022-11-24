e5358bd100bcbac4fce120952e6672e4cb555084
tkhwang <tkhwang@gmail.com>
2022-11-24 13:53:02 +0900

    Time: 136 ms (72.29%), Space: 50.6 MB (17.55%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.js
+++ 0072-edit-distance/0072-edit-distance.js
@@ -16,20 +16,16 @@ var minDistance = function(word1, word2) {
         if (j < 1) return i;
         if (cache[genKey(i, j)] !== undefined) return cache[genKey(i, j)];
         
-        let res = Infinity;
         if (word1[i-1] === word2[j-1]) {
-            res = Math.min(res, dp(i-1, j-1));
+            cache[genKey(i, j)] = dp(i-1, j-1);
         } else {
-            res = Math.min(
-                res,
-                 Math.min(
+            cache[genKey(i, j)] = Math.min(
                     dp(i-1, j) + 1,
                     dp(i, j-1) + 1,
                     dp(i-1,j-1) + 1
                 )
-            );
         }
-        return cache[genKey(i, j)] = res;
+        return cache[genKey(i, j)];
     }
     
     return dp(N1, N2)
