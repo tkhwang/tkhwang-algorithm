071d313bfa40b9c911c9235112f7a9a5f700da03
tkhwang <tkhwang@gmail.com>
2022-11-24 11:54:14 +0900

    Time: 1077 ms (44.36%), Space: 45.1 MB (56.78%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -11,25 +11,26 @@ var exist = function(board, word) {
     const directions = [ [1,0], [-1,0], [0,1], [0,-1] ];
     const isValid = (r, c) => !(r < 0 || r >= ROWS || c < 0 || c >= COLS);
     
+    const VISITED = "@";
+    
     const dfs = (r, c, index) => {
         if (board[r][c] !== word[index]) return false;
-        const org = board[r][c];
         if (index >= N - 1) return true;
+
+        board[r][c] = VISITED;
         
-        let res = false;
-        board[r][c] = "0"
         for (const [ dR, dC ] of directions) {
             const newR = r + dR;
             const newC = c + dC;
             
             if (!isValid(newR, newC)) continue;
-            // if (seen.has(genKey(newR, newC))) continue;
-            if (board[newR][newC] === "0") continue;
+            if (board[newR][newC] === VISITED) continue;
             
-            res = res || dfs(newR, newC, index + 1);
+            if (dfs(newR, newC, index + 1)) return true;
         }
-        board[r][c] = org;
-        return res;
+        board[r][c] = word[index];
+        
+        return false;
     }
     
     for (let r = 0; r < ROWS; r += 1) {
