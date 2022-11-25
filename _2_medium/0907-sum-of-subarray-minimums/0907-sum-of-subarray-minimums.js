9eb780cfce760d4fe43d10cc154e0d1bd2dc9463
tkhwang <tkhwang@gmail.com>
2022-11-25 19:16:27 +0900

    feat: :recycle: re-arrange by difficulty

--- _2_medium/0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.js
+++ _2_medium/0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.js
@@ -4,22 +4,20 @@
  */
 var sumSubarrayMins = function(arr) {
     const N = arr.length;
-
     const MOD = 7 + 10 ** 9;
+
     const stack = [];
     let sum = 0;
     
     for (let i = 0; i <= N; i += 1) {
-        while (stack.length > 0 && (i === N) || (arr[stack[stack.length - 1]] >= arr[i])) {
+        while (stack.length && (i === N || arr[stack.at(-1)] >= arr[i])) {
             const mid = stack.pop();
-            const left = stack.length > 0 ? stack[stack.length - 1] : -1;
-            const right = i;
+            const left = stack.length ? stack.at(-1) : -1;
             
-            const count = ((mid - left) * (right - mid)) % MOD;
-            sum += (count * arr[mid]) % MOD;
-            sum %= MOD;
+            sum += arr[mid] * (i - mid) * (mid - left) % MOD;
         }
-        stack.push(i);
+        stack.push(i)
     }
-    return sum;
+
+    return sum % MOD;
 };
