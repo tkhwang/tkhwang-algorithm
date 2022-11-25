39af701d39fa908cc8ec784874b93fbc4f5536ff
tkhwang <tkhwang@gmail.com>
2022-11-25 12:31:06 +0900

    Time: 188 ms (48.55%), Space: 49.2 MB (71.01%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.js
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.js
@@ -5,20 +5,19 @@
 var sumSubarrayMins = function(arr) {
     const N = arr.length;
     const MOD = 7 + 10 ** 9;
-    
+
     const stack = [];
     let sum = 0;
+    
     for (let i = 0; i <= N; i += 1) {
         while (stack.length && (i === N || arr[stack.at(-1)] >= arr[i])) {
             const mid = stack.pop();
-            const left = stack.length > 0 ? stack.at(-1) : -1;
-            const right = i;
+            const left = stack.length ? stack.at(-1) : -1;
             
-            const count = (mid - left) * (right - mid);
-            sum += (count * arr[mid]) % MOD;
+            sum += arr[mid] * (i - mid) * (mid - left) % MOD;
         }
         stack.push(i)
     }
-    
+
     return sum % MOD;
 };
