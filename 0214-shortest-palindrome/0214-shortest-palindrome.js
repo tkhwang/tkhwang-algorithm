c5a8181f23dbee16de701cefcbf63761d900817c
tkhwang <tkhwang@gmail.com>
2022-11-25 17:22:10 +0900

    Time: 113 ms (61.00%), Space: 44.2 MB (89.00%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var shortestPalindrome = function(s) {
+
+    const reverse = (str) => str.split("").reverse().join("");
+    
+    const recursive = (str) => {
+        let N = str.length;
+        let index = 0;
+        
+        for (let i = N - 1; i >= 0; i -= 1) {
+            if (str[i] === s[index]) index += 1;
+        }
+        
+        if (index === N) return str;
+
+        //  [0 ... [index ... N-1 ]]
+        
+        let remain = str.substr(index, N);
+        
+        return reverse(remain) + recursive(str.slice(0, index)) + str.slice(index);
+    }
+
+    return recursive(s)
+};
