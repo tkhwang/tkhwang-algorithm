525a2a4643dc030c78467b63d5ea1547dad1268d
tkhwang <tkhwang@gmail.com>
2022-11-25 19:30:43 +0900

    Time: 1416 ms (6.12%), Space: 180.1 MB (6.12%) - LeetHub

---
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxSubArrayLen = function(nums, k) {
+    const N = nums.length;
+    
+    const prefixSumMap = { 0: [ -1 ]  };
+    let max = -Infinity;
+    let sum = 0;
+    
+    for (let i = 0; i < N; i += 1) {
+        sum += nums[i];
+        
+        if (prefixSumMap[sum - k] !== undefined) {
+            const min = Math.min(...prefixSumMap[sum - k]);
+            if (max < i - min) max = i - min;
+        }
+        if (prefixSumMap[sum] === undefined) prefixSumMap[sum]= [];
+        prefixSumMap[sum].push(i)
+    }
+    return max === -Infinity ? 0 : max;
+};
