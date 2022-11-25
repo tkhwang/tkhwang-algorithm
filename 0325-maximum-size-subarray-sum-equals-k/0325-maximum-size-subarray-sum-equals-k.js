98ef817fc9c3e7553e056fc27b67c577f1b345cc
tkhwang <tkhwang@gmail.com>
2022-11-25 19:35:47 +0900

    Time: 205 ms (87.76%), Space: 79.7 MB (85.71%) - LeetHub

--- 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.js
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.js
@@ -6,19 +6,21 @@
 var maxSubArrayLen = function(nums, k) {
     const N = nums.length;
     
-    const prefixSumMap = { 0: [ -1 ]  };
     let max = -Infinity;
+    const map = new Map();
+    map.set(0,  -1)
+    
     let sum = 0;
     
-    for (let i = 0; i < N; i += 1) {
+    for (let i = 0; i < N; i += 1){
         sum += nums[i];
-        
-        if (prefixSumMap[sum - k] !== undefined) {
-            const min = Math.min(...prefixSumMap[sum - k]);
-            if (max < i - min) max = i - min;
+
+        if (map.has(sum - k)) {
+            if (max < i - map.get(sum - k)) max = i - map.get(sum - k);
         }
-        if (prefixSumMap[sum] === undefined) prefixSumMap[sum]= [];
-        prefixSumMap[sum].push(i)
+        
+        if (!map.has(sum)) map.set(sum, i);
     }
+    
     return max === -Infinity ? 0 : max;
 };
