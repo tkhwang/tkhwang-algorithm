1923aed41d1412543f75084a572618b7d13fb08f
tkhwang <tkhwang@gmail.com>
2022-11-25 20:11:56 +0900

    Time: 256 ms (36.23%), Space: 48.4 MB (72.46%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.js
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.js
@@ -6,40 +6,18 @@ var sumSubarrayMins = function(arr) {
     const N = arr.length;
     const MOD = 7 + 10 ** 9;
 
-    const minRight = Array(N).fill(N);
-    const minLeft  = Array(N).fill(-1);
-    
-    const stack = [ ];
-    for (let i = 0; i <= N; i += 1) {
-        while (stack.length && arr[stack.at(-1)] >= arr[i]) {
-            const pop = stack.pop();
-            minRight[pop] = i;
-        }
-        stack.push(i)
-    }
-    
-    stack.length = 0;
-    for (let i = N - 1; i >= 0; i -= 1) {
-        while (stack.length && arr[stack.at(-1)] > arr[i]) {
-            const pop = stack.pop();
-            minLeft[pop] = i;
-        }
-        stack.push(i)
-    }
-
-    console.log(minLeft);
-    console.log(minRight);
-    
+    const stack = [];
     let sum = 0;
     
-    for (let i = 0; i < N; i += 1) {
-        const left = i - minLeft[i];
-        const right = minRight[i] -i;
-        
-        console.log(left, i, right)
-        
-        sum += (arr[i] * left * right ) % MOD;
+    for (let right = 0; right <= N; right += 1) {
+        while (stack.length && (right === N || arr[stack.at(-1)] >= arr[right])) {
+            const mid = stack.pop();
+            const left = stack.length ? stack.at(-1) : -1;
+            
+            sum += arr[mid] * (right - mid) * (mid - left) % MOD;
+        }
+        stack.push(right)
     }
-    
+
     return sum % MOD;
 };
