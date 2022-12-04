907734b1e30adbd618cd86d4a0fcfb40690f7d89
tkhwang <tkhwang@gmail.com>
2022-12-04 16:27:10 +0900

    Time: 3797 ms (6.90%), Space: 91.4 MB (5.17%) - LeetHub

---
+++ 2256-minimum-average-difference/2256-minimum-average-difference.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumAverageDifference = function(nums) {
+    const N = nums.length;
+    
+    const prefixSumRight = Array(N+1).fill(0);
+    const prefixSumLeft  = Array(N+1).fill(0);
+    
+    let sum = 0;
+    for (let i = N - 1; i >= 0; i -= 1) {
+        sum += nums[i];
+        prefixSumRight[i] = sum
+    }
+    
+    sum = 0;
+    for (let i = 0; i <= N; i += 1) {
+        prefixSumLeft[i] = sum;
+        sum += nums[i];
+    }
+    
+    let min = Infinity;
+    let minIndex;
+    
+    for (let i = 0; i < N; i += 1) {
+        let left = Math.floor(prefixSumLeft[i+1]/(i+1));
+        let right = i === N - 1 ? 0 :
+                      Math.floor(prefixSumRight[i+1] / (N - i - 1));
+
+        const local = Math.abs(left - right);
+
+        console.log(left, right, local);
+        
+        if (min > local) {
+            min = local;
+            minIndex = i;
+        }
+    }
+    
+    return minIndex;
+};
