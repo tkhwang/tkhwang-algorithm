f4c7c53b83c92cb405b59c9cd97e30c063b3513d
tkhwang <tkhwang@gmail.com>
2022-12-07 14:07:47 +0900

    Time: 69 ms (93.54%), Space: 44.3 MB (34.09%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    const set = new Set();
+    
+    while (n !== 1) {
+        n = String(n)
+            .split("")
+            .map(Number)
+            .map((v) => v * v)
+            .reduce((a,b) => a + b, 0)
+        if (set.has(n)) return false;
+        set.add(n);
+    }
+    
+    return true;
+};
