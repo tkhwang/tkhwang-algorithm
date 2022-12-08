8d01cda01b16c4aea3c2050fac651a8ca9ed03ad
tkhwang <tkhwang@gmail.com>
2022-12-08 10:05:50 +0900

    Time: 68 ms (93.36%), Space: 44.1 MB (88.50%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var leafSimilar = function(root1, root2) {
+    const leaf1 = [];
+    const leaf2 = [];
+    
+    const dfs = (node, arr) => {
+        if (!node) return;
+        
+        dfs(node.left, arr);
+        
+        if (!node.left && !node.right) arr.push(node.val);
+        
+        dfs(node.right, arr);
+    }
+    
+    dfs(root1, leaf1);
+    dfs(root2, leaf2);
+    
+    return leaf1.length === leaf2.length && leaf1.every((v,i) => leaf2[i] === v);
+};
