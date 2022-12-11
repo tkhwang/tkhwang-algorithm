7a2f83c81551fe4a3e1719a2a82fa8faf54bcffb
tkhwang <tkhwang@gmail.com>
2022-12-11 14:38:11 +0900

    Time: 191 ms (100.00%), Space: 50.2 MB (100.00%) - LeetHub

---
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var deleteGreatestValue = function(grid) {
+    const [ ROWS, COLS ] = [ grid.length, grid[0].length ];
+    
+    for (let i = 0; i < grid.length; i += 1) {
+        grid[i].sort((a,b) => b - a);
+    }
+
+    console.log(grid);
+    
+    let res = 0;
+    
+    for (let c = 0; c < COLS; c += 1) {
+        let max = -Infinity;
+        for (let r = 0; r < ROWS; r += 1) {
+            max = Math.max(
+                max,
+                grid[r][c]
+            )
+        }
+        res += max;
+    }
+    
+    return res;
+};
