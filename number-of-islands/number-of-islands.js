07fd534134939630f41b6aa561d9b264b1d7e890
tkhwang <tkhwang@gmail.com>
2022-12-13 13:23:37 +0900

    Time: 131 ms, Memory: 45.5 MB - LeetHub

---
+++ number-of-islands/number-of-islands.js
@@ -0,0 +1,38 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    const [ ROWS, COLS ] = [ grid.length, grid[0].length ];
+    
+    let count = 0;
+    
+    const isValid = (r, c) => !(r < 0 || r >= ROWS || c < 0 || c >= COLS);
+    const directions = [ [1,0],[-1,0],[0,1],[0,-1] ];
+    
+    const dfs = (r, c) => {
+        if (grid[r][c] === "0") return;
+        grid[r][c] = "0";
+        
+        for (const [ dR, dC ] of directions) {
+            const newR = r + dR;
+            const newC = c + dC;
+
+            if (!isValid(newR, newC)) continue;
+            if (grid[newR][newC] === 0) continue;
+            
+            dfs(newR, newC);
+        }
+    }
+    
+    for (let r = 0; r < ROWS; r += 1) {
+        for (let c = 0; c < COLS; c += 1) {
+            if (grid[r][c] === "1") {
+                count += 1;
+                dfs(r, c)
+            }
+        }
+    }
+    
+    return count;
+};
