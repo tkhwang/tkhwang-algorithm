6a09e4d2ea0ff47eef296b8e763013097734096b
tkhwang <tkhwang@gmail.com>
2022-12-16 10:43:56 +0900

    Time: 144 ms, Memory: 119.5 MB - LeetHub

---
+++ construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+var buildTree = function(inorder, postorder) {
+    const dfs = (inorder, postorder) => {
+        if (inorder.length === 0 && postorder.length === 0) return null;
+        
+        const rootVal = postorder.at(-1);
+        const root = new TreeNode(rootVal);
+        
+        const mid = inorder.indexOf(rootVal);
+        
+        root.left = dfs(inorder.slice(0, mid), postorder.slice(0,mid));
+        root.right = dfs(inorder.slice(mid+1), postorder.slice(mid, postorder.length - 1));
+        
+        return root;
+    }
+    
+    return dfs(inorder, postorder)
+};
