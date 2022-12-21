7c9b3cb85b23f8ca63c59a0339f843047a94f090
tkhwang <tkhwang@gmail.com>
2022-12-21 21:21:18 +0900

    Time: 65 ms (95.54%), Space: 44.4 MB (48.60%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+    let sum = 0;
+    
+    const dfs = (node, isLeft) => {
+        if (!node) return 0;
+        if (!node.left && !node.right) {
+            return isLeft ? node.val : 0;
+        }
+            
+        return dfs(node.left, true) + dfs(node.right, false);
+    }
+    return dfs(root, false)
+};
