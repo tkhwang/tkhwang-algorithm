da8c4dad86c01e0117bcdef2ba38621b79886a7f
tkhwang <tkhwang@gmail.com>
2022-12-22 16:25:47 +0900

    refactor(arrange): :recycle: re-arrange by difficulty

--- _1_easy/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ _1_easy/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -10,12 +10,12 @@
  * @return {ListNode}
  */
 var middleNode = function(head) {
-    let fast = head;
     let slow = head;
+    let fast = head;
     
     while (fast && fast.next) {
-        fast = fast.next.next;
         slow = slow.next;
+        fast = fast.next.next;
     }
     
     return slow;
