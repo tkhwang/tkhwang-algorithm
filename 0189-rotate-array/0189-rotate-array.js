3f0ea7d152c297eadd53ef80a100cccceb001484
tkhwang <tkhwang@gmail.com>
2022-12-26 22:17:05 +0900

    Time: 89 ms (98.33%), Space: 51 MB (91.22%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    const N = nums.length;
+    
+    const deltaK = k % N;
+    return nums.splice(0, 0, ...nums.splice(N - deltaK))
+};
