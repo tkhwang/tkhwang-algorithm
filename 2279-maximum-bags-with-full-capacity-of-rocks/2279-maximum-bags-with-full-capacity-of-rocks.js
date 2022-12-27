19fade300f076831f9297dbdb836663fc0a1cae1
tkhwang <tkhwang@gmail.com>
2022-12-27 10:29:14 +0900

    Time: 221 ms (72.00%), Space: 58.1 MB (40.00%) - LeetHub

---
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} capacity
+ * @param {number[]} rocks
+ * @param {number} additionalRocks
+ * @return {number}
+ */
+var maximumBags = function(capacity, rocks, additionalRocks) {
+    const diff = capacity.map((v,i) => capacity[i] - rocks[i]);
+    
+    diff.sort((a,b) => a - b);
+    
+    console.log(diff);
+    
+    const N = diff.length;
+    let i = 0;
+    let count = 0;
+    
+    while (i < N && additionalRocks > 0) {
+        if (diff[i] > 0) {
+            if (diff[i] > additionalRocks) break;
+            additionalRocks -= diff[i];
+        }
+        count += 1;
+        i += 1;
+    }
+    
+    return count;
+};
