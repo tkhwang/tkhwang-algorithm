187286d35a0ccf4ee7c9de5310585be63d48d4f2
tkhwang <tkhwang@gmail.com>
2022-12-29 09:45:12 +0900

    refactor(arrange): :recycle: re-arrange by difficulty

--- _2_medium/1306-jump-game-iii/1306-jump-game-iii.js
+++ _2_medium/1306-jump-game-iii/1306-jump-game-iii.js
@@ -4,28 +4,34 @@
  * @return {boolean}
  */
 var canReach = function(arr, start) {
-
+    const N = arr.length;
+    
     const dfs = (cur, seen) => {
         if (arr[cur] === 0) return true;
-        
+    
         let res = false;
         let next = cur + arr[cur];
-        if (!seen.has(next)) {
-            seen.add(next);
-            res = res || dfs(next, seen);
-            seen.delete(next);
+        
+        if (next < N) {
+            if (!seen.has(next)) {
+                seen.add(next);
+                res = res || dfs(next, seen);
+                seen.delete(next);
+            }
         }
         next = cur - arr[cur];
-        if (!seen.has(next)) {
-            seen.add(next);
-            res = res || dfs(next, seen);
-            seen.delete(next);
+        if (0 <= next) {
+            if (!seen.has(next)) {
+                seen.add(next);
+                res = res || dfs(next, seen);
+                seen.delete(next);
+            }
         }
+        
         return res;
     }
-
+    
     const seen = new Set();
     seen.add(start)
-    
-    return dfs(start, seen);
+    return dfs(start, seen)
 };
