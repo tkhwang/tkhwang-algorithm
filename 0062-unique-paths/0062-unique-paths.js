3bf44efd402d94c8bd34632772a47755b20b7614
tkhwang <tkhwang@gmail.com>
2022-12-30 14:29:23 +0900

    Time: 75 ms (71.94%), Space: 41.5 MB (96.59%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -6,13 +6,11 @@
 var uniquePaths = function(m, n) {
     const [ ROWS, COLS ] = [ m, n ];
     
-    const dp = Array(ROWS).fill(null).map((_) => Array(COLS).fill(0));
-    dp[0][0] = 1;
+    const dp = Array(ROWS).fill(null).map((_) => Array(COLS).fill(1));
 
-    for (let r = 0; r < ROWS; r += 1) {
-        for (let c = 0; c < COLS; c += 1) {
-            if (c > 0) dp[r][c] += dp[r][c-1];
-            if (r > 0) dp[r][c] += dp[r-1][c];
+    for (let r = 1; r < ROWS; r += 1) {
+        for (let c = 1; c < COLS; c += 1) {
+            dp[r][c] = dp[r-1][c] + dp[r][c-1]
         }
     }
     
