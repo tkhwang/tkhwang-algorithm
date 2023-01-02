08f16dd3295a2f7bf365139b695b08ee871d11ab
tkhwang <tkhwang@gmail.com>
2023-01-02 13:33:47 +0900

    Time: 58 ms (88.00%), Space: 42 MB (48.00%) - LeetHub

---
+++ 1056-confusing-number/1056-confusing-number.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var confusingNumber = function(n) {
+    const N = String(n).length;
+    
+    const set = new Set([ '0', '1', '6', '8', '9' ]);
+    
+    const obj = {
+        "0": "0",
+        "1": "1",
+        "6": "9",
+        "8": "8",
+        "9": "6",
+    }
+    
+    if (N === 1) return [ 6, 9 ].includes(n);
+
+    let rotated = "";
+    for (const ch of String(n).split("")) {
+        if (!set.has(ch)) return false;
+        rotated = obj[ch] + rotated;
+    }
+    
+    console.log(n);
+    console.log(rotated)
+    
+    return String(n) !== rotated
+};
