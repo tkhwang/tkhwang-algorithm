a4c31f7e3b490b58e6f1ca51c6e9fbb2df0eaafc
tkhwang <tkhwang@gmail.com>
2023-01-02 10:36:19 +0900

    Time: 366 ms (90.89%), Space: 82.9 MB (49.56%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(N) {
+  const seen = Array(N + 1).fill(false);
+  let count = 0;
+
+  for (let i = 2; i < N; i += 1) {
+    if (seen[i]) continue;
+
+    count += 1;
+
+    for (let j = i * i; j < N; j += i) {
+      seen[j] = true;
+    }
+  }
+
+  return count;
+};
