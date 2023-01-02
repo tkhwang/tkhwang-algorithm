4ddbb6dbf9cf9a62899dc2ad8eec070de2965e5a
tkhwang <tkhwang@gmail.com>
2023-01-02 15:19:41 +0900

    Time: 76 ms (93.75%), Space: 47.3 MB (56.25%) - LeetHub

---
+++ 0272-closest-binary-search-tree-value-ii/0272-closest-binary-search-tree-value-ii.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @param {number} k
+ * @return {number[]}
+ */
+var closestKValues = function(root, target, k) {
+
+    const res = [];
+    
+    const dfs = (node) => {
+        if (!node) return;
+        
+        res.push(node.val);
+        
+        dfs(node.left);
+        dfs(node.right);
+    }
+    
+    dfs(root);
+    
+    res.sort((a,b) => Math.abs(a - target) - Math.abs(b - target));
+    
+    console.log(res)
+    
+    return res.slice(0, k);
+};
