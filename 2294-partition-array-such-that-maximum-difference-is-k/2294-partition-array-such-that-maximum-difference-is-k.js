3f95686b907a603ba9497bf5562781ac28f6b98a
tkhwang <tkhwang@gmail.com>
2023-01-03 18:27:51 +0900

    Time: 429 ms (46.30%), Space: 54.6 MB (40.74%) - LeetHub

---
+++ 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var partitionArray = function(nums, k) {
+    const N = nums.length;
+    
+    nums.sort((a,b) => a - b);
+    
+    console.log(nums);
+    
+    let count = 0;
+    let min = nums[0];
+    for (let i = 1; i < N; i += 1) {
+        const cur = nums[i];
+        if (cur <= min + k) {
+            
+        } else {
+            min = cur;
+            count += 1;
+        }
+    }
+    return count + 1;
+};
