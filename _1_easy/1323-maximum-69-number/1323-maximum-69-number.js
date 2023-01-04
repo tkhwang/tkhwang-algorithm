c210537d74fd730577f610a7b609d62b8186224b
tkhwang <tkhwang@gmail.com>
2023-01-04 12:23:00 +0900

    refactor(arrange): :recycle: re-arrange by difficulty

--- _1_easy/1323-maximum-69-number/1323-maximum-69-number.js
+++ _1_easy/1323-maximum-69-number/1323-maximum-69-number.js
@@ -3,12 +3,13 @@
  * @return {number}
  */
 var maximum69Number  = function(_num) {
-    const num = String(_num).split("");
+    const num = String(_num).split("").map(Number);
     
-    const N = num.length;
+    const N = String(num).length;
+    
     for (let i = 0; i < N; i += 1) {
-        if (num[i] === '6') {
-            num[i] = '9'
+        if (num[i] === 6) {
+            num[i] = 9;
             break;
         }
     }
--- _2_medium/2126-destroying-asteroids/2126-destroying-asteroids.js
+++ _2_medium/2126-destroying-asteroids/2126-destroying-asteroids.js
@@ -4,21 +4,12 @@
  * @return {boolean}
  */
 var asteroidsDestroyed = function(mass, asteroids) {
-    const n = asteroids.length;
-    
-    const dp = Array(n).fill(0);
-    
     asteroids.sort((a,b) => a - b);
     
-    if(asteroids[0] > mass) return false;
-    
-    dp[0] = mass + asteroids[0];
-    
-    for (let i = 1; i < n; i += 1) {
-        if (dp[i-1] < asteroids[i]) return false;
+    for (const [ i, asteroid ] of asteroids.entries()) {
+        if (mass < asteroid) return false;
         
-        dp[i] = dp[i-1] + asteroids[i];
+        mass += asteroid;
     }
-    
     return true;
 };
