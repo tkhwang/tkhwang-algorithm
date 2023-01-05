4cd33d0473c354aa931f0dbeb78a387ebeaebce1
tkhwang <tkhwang@gmail.com>
2023-01-05 11:39:28 +0900

    Time: 127 ms (18.38%), Space: 44.1 MB (66.87%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.js
+++ 0252-meeting-rooms/0252-meeting-rooms.js
@@ -8,9 +8,8 @@ var canAttendMeetings = function(intervals) {
     
     intervals.sort((a,b) => a[1] - b[1]);
     
-    let [ , prvEnd ] = intervals[0];
-    
     for (let i = 1; i < N; i += 1) {
+        let [ prvStart, prvEnd ] = intervals[i-1];
         const [ start, end ] = intervals[i];
         
         if (start < prvEnd) return false;
