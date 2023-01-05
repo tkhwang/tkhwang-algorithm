430cefa10f17dee897007719b56a73fb3e53c7d4
tkhwang <tkhwang@gmail.com>
2023-01-05 12:52:04 +0900

    Time: 131 ms (37.50%), Space: 45.3 MB (33.62%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
@@ -6,13 +6,10 @@ var minMeetingRooms = function(intervals) {
     const N = intervals.length;
     
     intervals.sort((a,b) => a[0] - b[0]);
-    console.log(intervals);
     
     const minHeap = new MinPriorityQueue({ compare: (a,b) => a - b });
     minHeap.enqueue(intervals[0][1]);
     
-    let max = -Infinity;
-    let rooms = 1;
     for (let i = 1; i < N; i += 1) {
         const [ start, end ] = intervals[i];
         const earliestEnd = minHeap.front();
