808fab3a86d700f35b69e0490f1e8315c3368233
tkhwang <tkhwang@gmail.com>
2023-01-06 14:24:24 +0900

    Time: 110 ms (46.25%), Space: 45.1 MB (30.00%) - LeetHub

--- 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.js
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.js
@@ -5,17 +5,34 @@
  * @return {number[]}
  */
 var arraysIntersection = function(arr1, arr2, arr3) {
-    const freq = {};
-    
-    for (const arr of [ arr1, arr2, arr3 ]) {
-        for (const num of arr) {
-            freq[num] = (freq[num] || 0) + 1;
+
+    const bisect = (array, target) => {
+        const N = array.length;
+        
+        // [left, right]
+        let left = 0;
+        let right = N-1;
+        
+        while (left <= right) {
+            const mid = Math.floor((left + right)/2);
+            
+            if (array[mid] === target) return mid;
+            else if (array[mid] > target) {
+                right = mid - 1;    
+            } else if (array[mid] < target) {
+                left = mid + 1;
+            }
         }
-    }
+        return -1;
+    }    
     
     const res = [];
-    for (const key in freq) {
-        if (freq[key] >= 3) res.push(key);
+    
+    for (const num of arr1) {
+        const second = bisect(arr2, num);
+        const third = bisect(arr3, num);
+        
+        if (second !== -1 && third !== -1) res.push(num);
     }
     
     return res;
