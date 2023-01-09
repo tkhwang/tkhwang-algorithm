e8fdcd60006b369ab9be4be019d281cd9512ef9e
tkhwang <tkhwang@gmail.com>
2023-01-09 16:57:56 +0900

    Time: 552 ms (33.98%), Space: 57.1 MB (76.45%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    const set = new Set(nums);
+    
+    let max = 0;
+    
+    for (let num of nums) {
+        if (!set.has(num - 1)) {
+            let k = 0;
+            while (set.has(num + k)) {
+                k += 1;
+            }
+            if (max < k) max = k;
+        }
+    }
+    
+    return max;
+};
