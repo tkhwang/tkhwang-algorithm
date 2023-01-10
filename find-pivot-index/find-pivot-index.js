31637f6d62f8fbb92142cd2aa96845ef5dca94c3
tkhwang <tkhwang@gmail.com>
2023-01-10 15:15:02 +0900

    Time: 85 ms, Memory: 45.2 MB - LeetHub

--- find-pivot-index/find-pivot-index.js
+++ find-pivot-index/find-pivot-index.js
@@ -4,24 +4,24 @@
  */
 var pivotIndex = function(nums) {
     const N = nums.length;
-
-    const sumLeft = Array(N).fill(0);
-    const sumRight = Array(N).fill(0);
+    
+    const sumFromLeft = Array(N).fill(0);
+    const sumFromRight = Array(N).fill(0);
     
     let sum = 0;
     for (let i = 0; i < N; i += 1) {
-        sumLeft[i] = sum;
         sum += nums[i];
+        sumFromLeft[i] = sum;
     }
+    
     sum = 0;
     for (let i = N - 1; i >= 0; i -= 1) {
-        sumRight[i] = sum;
         sum += nums[i];
+        sumFromRight[i] = sum;
     }
     
     for (let i = 0; i < N; i += 1) {
-        if (sumLeft[i] === sumRight[i]) return i;
+        if (sumFromLeft[i] === sumFromRight[i]) return i;
     }
-    
     return -1;
 };
