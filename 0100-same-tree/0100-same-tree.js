443f64116b97d6b671e9418479a7164d3536663a
tkhwang <tkhwang@gmail.com>
2023-01-10 09:02:58 +0900

    Time: 76 ms (58.43%), Space: 42.1 MB (80.30%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    const dfs = (p, q) => {
+        if (!p && !q) return true;
+        if (!p && q || p && !q) return false;
+        if (p.val !== q.val) return false;
+        
+        return dfs(p.left, q.left) && dfs(p.right, q.right);
+    }
+    
+    return dfs(p, q)
+};
