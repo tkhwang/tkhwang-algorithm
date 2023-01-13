349b7dbc06a493f74456dbc968531baf16c5f4df
tkhwang <tkhwang@gmail.com>
2023-01-13 16:41:17 +0900

    Time: 82 ms (85.48%), Space: 46.4 MB (88.71%) - LeetHub

---
+++ 1469-find-all-the-lonely-nodes/1469-find-all-the-lonely-nodes.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var getLonelyNodes = function(root) {
+    const res = [];
+    
+    const dfs = (node, isLonely) => {
+        if (!node) return;
+
+        if (isLonely) res.push(node.val);
+        
+        const isLocalLonely = (node.left && !node.right || !node.left && node.right);
+
+        dfs(node.left, isLocalLonely);
+        dfs(node.right, isLocalLonely);
+    }
+    
+    dfs(root, false);
+    
+    return res;
+};
