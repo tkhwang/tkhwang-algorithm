84c0684603ad77c41d8718d8d966cc13c684f2a6
tkhwang <tkhwang@gmail.com>
2023-01-14 13:14:09 +0900

    Time: 377 ms (31.91%), Space: 67.2 MB (8.51%) - LeetHub

---
+++ 0582-kill-process/0582-kill-process.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} pid
+ * @param {number[]} ppid
+ * @param {number} kill
+ * @return {number[]}
+ */
+var killProcess = function(pid, ppid, kill) {
+    const graph = {};
+    
+    const N = pid.length;
+    
+    for (let i = 0; i < N; i += 1) {
+        const cur = pid[i];
+        const parent = ppid[i];
+        
+        if (parent === 0) continue;
+        
+        if (graph[parent] === undefined) graph[parent] = [];
+        graph[parent].push(cur);
+    }
+    
+    console.log(graph);
+  
+    const res = [];
+    
+    const dfs = (cur) => {
+        res.push(cur);
+        
+        if (graph[cur] === undefined) return;
+        
+        for (const child of graph[cur]) {
+            dfs(child);
+        }
+    }
+    
+    dfs(kill);
+    
+    return res;
+};
