2e5473f6fb6fc7ed0e928a8c8aa687d1fcc0d2b6
tkhwang <tkhwang@gmail.com>
2023-01-27 10:27:00 +0900

    Time: 1683 ms (5.66%), Space: 92.2 MB (5.66%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findAllConcatenatedWordsInADict = function(words) {
+    const trie = {};
+    for (const word of words) {
+        let node = trie;
+        for (const ch of word) {
+            if (node[ch] === undefined) node[ch] = {};
+            node = node[ch]
+        }
+        node["word"] = word
+    }
+    
+    // console.log(JSON.stringify(trie));
+    
+    const dfs = (word, count = 0) => {
+        console.log(word, count);
+        if (!word) return count > 1;
+        
+        let node = trie;
+        
+        for (const [ i, ch ] of word.split("").entries()) {
+            if (node[ch] === undefined) return false;
+            node = node[ch];
+            if (node["word"] === word.slice(0, i + 1)) {
+                if(dfs(word.slice(i+1), count + 1)) return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    const res = [];
+    
+    for (const word of words) {
+        console.log("word => ", word);
+        if (dfs(word)) res.push(word);
+    }     
+    
+    return res;
+};
