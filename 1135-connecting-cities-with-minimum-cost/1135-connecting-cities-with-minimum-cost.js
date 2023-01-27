f2a1c288328252790c300b151919b26b1dd48f98
tkhwang <tkhwang@gmail.com>
2023-01-27 15:30:25 +0900

    Time: 439 ms (20.00%), Space: 74.5 MB (10.00%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.js
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.js
@@ -4,57 +4,33 @@
  * @return {number}
  */
 var minimumCost = function(n, connections) {
-    connections.sort((a,b) => a[2] - b[2] );
-
-    const uf = new UnionFind(n);
-    
-    let res = 0;
+    const graph = Array(n + 1).fill(null).map((_) => []);
     
     for (const [ u, v, cost ] of connections) {
-        if (uf.find(u) !== uf.find(v)) {
-            res += cost;
-            uf.union(u, v);
-        }
+        graph[u].push([ v, cost ]);
+        graph[v].push([ u, cost ]);
     }
     
-    return uf.getComponents() === 1 ? res : -1;
-};
+    const visited = new Set();
+    let res = 0;
+    
+    const minHeap = new MinPriorityQueue({ compare: (a,b) => a[0] - b[0] });
+    minHeap.enqueue([ 0, 1 ]);
+    
+    while (minHeap.size()) {
+        const [ cost, cur ] = minHeap.dequeue();
 
-class UnionFind {
-    constructor(n) {
-        this.root = Array(n).fill(null).map((_,i) => i);
-        this.rank = Array(n).fill(1);
-        this.components = n;
-    }
-    
-    find(node) {
-        if (node === this.root[node]) return node;
-        return this.root[node] = this.find(this.root[node]);
-    }
-    
-    union(x, y) {
-        const rootX = this.find(x);
-        const rootY = this.find(y);
+        if (visited.has(cur)) continue;
+
+        res += cost;
+        visited.add(cur);
         
-        if (rootX === rootY) return false;
-        
-        if (this.rank[rootX] < this.rank[rootY]) {
-            this.root[rootX] = rootY;
-        } else if (this.rank[rootX] > this.rank[rootY]) {
-            this.root[rootY] = rootX;
-        } else {
-            this.root[rootY] = rootX;
-            this.rank[rootX] += 1;
+        for (const [ next, nextCost ] of graph[cur]) {
+            if (visited.has(next)) continue;
+            
+            minHeap.enqueue([ nextCost, next ]);
         }
-        this.components -= 1;
-        return true
     }
     
-    connected(x,y) {
-        return this.find(x) === this.find(y)
-    }
-    
-    getComponents() {
-        return this.components;
-    }
-}
+    return visited.size === n ? res : -1;
+};
