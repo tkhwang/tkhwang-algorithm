f4bfb53491e77ebd87aa25f4d25f05a46954a7af
tkhwang <tkhwang@gmail.com>
2023-01-30 15:46:48 +0900

    Time: 119 ms (76.84%), Space: 51.3 MB (46.82%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[][]} times
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var networkDelayTime = function(times, n, k) {
+    const graph = {};
+    for (const [ u, v, cost ] of times) {
+        if (graph[u] === undefined) graph[u] = [];
+        graph[u].push([ v, cost ]);
+    }
+    
+    const costs = Array(n + 1).fill(Infinity);
+    costs[k] = 0;
+    costs[0] = 0;
+    
+    // [ cost, next ]
+    const minHeap = new MinPriorityQueue({ compare: (a,b) => a[1] - b[1] });
+    minHeap.enqueue([ k, 0 ]);
+
+    while (minHeap.size()) {
+        const [ cur, cost ] = minHeap.dequeue();
+        
+        if (costs[cur] !== cost) continue;
+        if (graph[cur] === undefined) continue;
+        
+        for (const [ next, nextCost ] of graph[cur]) {
+            if (costs[next] > costs[cur] + nextCost) {
+                costs[next] = costs[cur] + nextCost;
+                minHeap.enqueue([ next, costs[next] ]);
+            }            
+        }
+    }
+    
+    const max = Math.max(...costs);
+    return max === Infinity ? -1 : max;
+};
