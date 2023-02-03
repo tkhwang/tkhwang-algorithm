bca909c02e6160674111044eeb9692ce2bbe614e
tkhwang <tkhwang@gmail.com>
2023-02-03 11:51:32 +0900

    refactor(arrange): :recycle: re-arrange by difficulty

--- _1_easy/0035-search-insert-position/0035-search-insert-position.js
+++ _1_easy/0035-search-insert-position/0035-search-insert-position.js
@@ -4,23 +4,62 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-
-    const bisect = (array, target) => {
-        let left = 0;
-        let right = array.length - 1;
+    const N = nums.length;
+    
+    // nums.sort((a,b) => a - b);
+    
+    const merge = (left, right) => {
+        const res = [];
         
-        while (left <= right) {
-            const mid = Math.floor((left + right)/2);
-            
-            if (array[mid] === target) return mid;
-            else if (array[mid] < target) {
-                left = mid + 1;
-            } else if (array[mid] > target) {
-                right = mid - 1;
+        const NL = left.length;
+        const NR = right.length;
+        
+        let i = 0;
+        let j = 0;
+        
+        while (i < NL && j < NR) {
+            if (left[i] <= right[j]) {
+                res.push(left[i]);
+                i += 1;
+            } else {
+                res.push(right[j]);
+                j += 1;
             }
         }
-        return left;
+        
+        return [
+            ...res,
+            ...left.slice(i),
+            ...right.slice(j)
+        ]
     }
     
-    return bisect(nums, target)
+    const mergeSort = (array) => {
+        if (array.length < 2) return array;
+            
+        const mid = Math.floor(array.length / 2);
+        
+        return merge(
+            mergeSort(array.slice(0, mid)),
+            mergeSort(array.slice(mid))
+        )
+    }
+    
+    mergeSort(nums)
+    
+    let left = 0;
+    let right = N- 1;
+    
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        
+        if (nums[mid] === target) return mid;
+        else if (nums[mid] < target) {
+            left = mid + 1;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        }
+    }
+    
+    return left;
 };
