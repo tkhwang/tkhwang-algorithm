72377a2ec22b44a0782e14b746da6680edf045ad
tkhwang <tkhwang@gmail.com>
2023-02-04 14:39:48 +0900

    Time: 140 ms (85.71%), Space: 52.5 MB (62.86%) - LeetHub

---
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} maxOperations
+ * @return {number}
+ */
+var minimumSize = function(nums, maxOperations) {
+    
+    /*
+                     ------------
+                     |
+        -------------| 
+                      left   ... right
+     */
+    
+    let left = 1;
+    let right = 10 ** 9;
+    
+    const isOK = (max) => {
+        let sum = 0;
+        for (const num of nums) {
+            sum += Math.floor((num - 1) / max);
+        }
+        return sum <= maxOperations;
+    }
+    
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+        
+        if (isOK(mid)) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left
+};
