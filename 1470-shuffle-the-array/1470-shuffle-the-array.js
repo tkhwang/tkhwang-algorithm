f6bdb50c23af071308a134916915fe84e737c3f7
tkhwang <tkhwang@gmail.com>
2023-02-06 09:02:05 +0900

    Time: 84 ms (37.51%), Space: 44.7 MB (35.37%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    const res = [];
+    
+    for (let i = 0; i < n; i += 1) {
+        res.push(nums[i]);
+        res.push(nums[i + n]);
+    }    
+    
+    return res;
+};
