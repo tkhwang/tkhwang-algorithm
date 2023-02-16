2d9526a18cd020a89766450540ac2309cd0f0832
tkhwang <tkhwang@gmail.com>
2023-02-16 11:03:11 +0900

    Time: 429 ms (42.42%), Space: 80.1 MB (46.97%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number} n
+ * @param {number} headID
+ * @param {number[]} manager
+ * @param {number[]} informTime
+ * @return {number}
+ */
+var numOfMinutes = function(n, headID, manager, informTime) {
+    const graph = {};
+    for (const [ user, boss ] of manager.entries()) {
+        if (boss === -1) continue;
+        
+        if (graph[boss] === undefined) graph[boss] = [];
+        graph[boss].push(user);
+    }
+    
+
+    let max = 0;
+    
+    const bfs = (start) => {
+        const queue = [ [ start, 0 ] ];
+        
+        while (queue.length) {
+            const len = queue.length;
+            
+            for (let i = 0; i < len; i += 1) {
+                const [ cur, time ] = queue.shift();
+                
+                if (graph[cur] === undefined) continue;
+                
+                for (const sub of graph[cur]) {
+                    max = Math.max(max, time + informTime[cur]);
+                    queue.push([ sub, time + informTime[cur] ])
+                }
+            }
+        }
+    }
+    
+    bfs(headID);
+    
+    return max;
+};
