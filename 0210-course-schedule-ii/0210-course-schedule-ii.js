1084044b0fb62aa31d8e0eede991b141c592c77e
tkhwang <tkhwang@gmail.com>
2023-02-18 15:09:34 +0900

    Time: 92 ms (53.16%), Space: 48 MB (30.02%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+var findOrder = function(N, prerequisites) {
+    const graph = {};
+    const indegrees = Array(N).fill(0);
+    
+    for (const [ course, precourse ] of prerequisites) {
+        if (graph[precourse] === undefined) graph[precourse] = [];
+        graph[precourse].push(course);
+        indegrees[course] += 1;
+    }
+    
+    const queue = [];
+    const orders = [];
+    
+    for (let i = 0; i < N; i += 1) {
+        if (indegrees[i] === 0) queue.push(i);
+    }
+    
+    while (queue.length) {
+        const len = queue.length;
+        
+        for (let i = 0; i < len; i += 1) {
+            const cur = queue.shift();
+            orders.push(cur);
+
+            if (graph[cur] === undefined) continue;
+            for (const next of graph[cur]) {
+                indegrees[next] -= 1;
+                if (indegrees[next] === 0) queue.push(next);
+            }
+        }
+    }
+    return orders.length === N ? orders : [];
+};
