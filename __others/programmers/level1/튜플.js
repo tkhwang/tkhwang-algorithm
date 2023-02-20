16ec82576840966f91a389ff4cc2a8fb3d54dff0
tkhwang <tkhwang@gmail.com>
2023-02-20 12:40:45 +0900

    refactor(programmers): :recycle: refactor use hashmap instead of array

--- __others/programmers/level1/튜플.js
+++ __others/programmers/level1/튜플.js
@@ -6,14 +6,15 @@ function solution(s) {
   const N = s.length;
 
   const arr = s.slice(2, N - 2).split("},{");
-  const res = [];
+  const map = new Map();
 
   for (const str of arr.sort((a, b) => a.length - b.length)) {
     for (const num of str.split(",")) {
-      if (res.includes(+num)) continue;
-      res.push(+num);
+      if (map.has(+num)) continue;
+
+      map.set(+num, +num);
     }
   }
 
-  return res;
+  return Array.from(map.keys());
 }
