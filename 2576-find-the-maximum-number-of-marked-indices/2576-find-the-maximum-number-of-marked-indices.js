9fdb60d1e52740dc1e1b3758fe1136ea0ab2eb6e
tkhwang <tkhwang@gmail.com>
2023-02-26 18:04:27 +0900

    Time: 1034 ms (100.00%), Space: 65.5 MB (100.00%) - LeetHub

---
+++ 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxNumOfMarkedIndices = function(nums) {
+    const N = nums.length;
+    
+    nums.sort((a,b) => a - b);
+    
+    let right = N - 1;
+    let left = Math.floor(N/2) - 1;
+    
+    let count = 0;
+    
+    while (left >= 0 && Math.floor(N/2) - 1 < right) {
+        console.log(left, right, nums[left], nums[right]);
+        
+        if (nums[left] * 2 <= nums[right]) {
+            count += 2;
+            right -= 1;
+            left -= 1;
+        } else if (nums[left] * 2 > nums[right]) {
+            left -= 1;
+        }
+    }
+    
+    return count;
+};
