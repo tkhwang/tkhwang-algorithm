85d1a7f0e56577192b42bae08b996ea47dfa287d
tkhwang <tkhwang@gmail.com>
2023-02-27 11:46:59 +0900

    Time: 1602 ms (100.00%), Space: 77.1 MB (100.00%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.js
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.js
@@ -1,56 +1,47 @@
-const minimumTime = function(grid) {
-    const numRows = grid.length;
-    const numCols = grid[0].length;
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minimumTime = function(grid) {
+    const [ ROWS, COLS ] = [ grid.length, grid[0].length ];
+    
+    if (grid[0][1] > 1 && grid[1][0] > 1) return -1;
+    
+    const directions = [ [1,0], [-1,0], [0,1], [0,-1] ];
+    const isValid = (r, c) => !(r < 0 || r >= ROWS || c < 0 || c >= COLS);
+    
+    const seen = new Set();
+    const genKey = (r, c) => `${r}:${c}`;
+    
+    // [ cost, r, c ]
+    const minHeap = new MinPriorityQueue({ compare: (a,b) => a[0] - b[0] });
+    minHeap.enqueue([ 0, 0, 0 ]);
 
-    // Check if the start and end points are reachable.
-    if (grid[0][1] > 1 && grid[1][0] > 1) {
-        return -1;
-    }
+    while (minHeap.size()) {
+        let [ time, r, c ] = minHeap.dequeue();
 
-    const visited = new Array(numRows)
-        .fill()
-        .map(() => new Array(numCols).fill(false));
+        // console.log(r, c, time);
+        
+        if (r === ROWS - 1 && c === COLS - 1) return time;
+        if (seen.has(genKey(r, c))) continue;
+        
+        seen.add(genKey(r, c));
+        time += 1;
+        
+        for (const [ dR, dC ] of directions) {
+            const newR = r + dR;
+            const newC = c + dC;
+            
+            if (!isValid(newR, newC)) continue;
 
-    const priorityQueue = new MinPriorityQueue({ priority: ([time]) => time });
-
-    if (grid[0][0] === 0) {
-        priorityQueue.enqueue([0, 0, 0]);
-    }
-
-    const directions = [[1, 0], [0, -1], [0, 1], [-1, 0]];
-
-    while (!priorityQueue.isEmpty()) {
-        const [time, currentRow, currentCol] = priorityQueue.dequeue().element;
-
-        if (currentRow === numRows - 1 && currentCol === numCols - 1) {
-            return time;
-        }
-
-        if (visited[currentRow][currentCol]) {
-            continue;
-        }
-
-        visited[currentRow][currentCol] = true;
-
-        for (const [rowOffset, colOffset] of directions) {
-            const newRow = currentRow + rowOffset;
-            const newCol = currentCol + colOffset;
-
-            if (
-                newRow < 0 ||
-                newCol < 0 ||
-                newRow >= numRows ||
-                newCol >= numCols ||
-                visited[newRow][newCol]
-            ) {
-                continue;
+            if (grid[newR][newC] <= time) {
+                minHeap.enqueue([ time, newR, newC ]);
+            } else {
+                const isOdd = (grid[newR][newC] - time) % 2 === 1;
+                const timeReq = grid[newR][newC] + isOdd;
+                minHeap.enqueue([ timeReq, newR, newC ]);
             }
-
-            const isEven = (grid[newRow][newCol] - time) % 2 === 0;
-            const timeReq = Math.max(grid[newRow][newCol] + isEven, time + 1);
-            priorityQueue.enqueue([timeReq, newRow, newCol]);
-        }
+        }    
     }
-
     return -1;
 };
