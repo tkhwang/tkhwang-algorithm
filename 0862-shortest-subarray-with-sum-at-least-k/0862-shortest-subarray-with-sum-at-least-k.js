a2fba3b83f9ae43aeaca83bf5df1c442eaec4e88
tkhwang <tkhwang@gmail.com>
2023-02-27 23:21:08 +0900

    Time: 581 ms (18.42%), Space: 70 MB (10.53%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.js
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.js
@@ -11,21 +11,46 @@ var shortestSubarray = function(nums, k) {
         array.push(num + array.at(-1));
     }
     
-    const queue = [];
-    let res = Infinity;
+    const prefixSum = Array(N + 1).fill(0);
+    for (let i = 0; i < N; i += 1) {
+        prefixSum[i+1] = prefixSum[i] + nums[i];
+    }
+   
+    console.log(prefixSum);
     
-    for (const [ i, num ] of array.entries()) {
-        if (queue.length === 0) {
-            queue.push(i);
-        } else {
-            while (queue.length && array[queue.at(-1)] > num) queue.pop();
-            while (queue.length && array[queue.at(0)] <= num - k) {
+    let res = Infinity;
+
+    const queue = [];
+    
+    for (const [ i, pre ] of prefixSum.entries()) {
+        // if (queue.length === 0) {
+            // queue.push(i)
+        // } else {
+            while (queue.length && prefixSum[queue.at(-1)] > pre) queue.pop();
+            while (queue.length && prefixSum[queue.at(0)] <= pre - k) {
                 res = Math.min(res, i - queue.at(0));
                 queue.shift();
             }
             queue.push(i);
-        }
+        // }
     }
+    
+    
+    
+//     const queue = [];
+
+//     for (const [ i, num ] of array.entries()) {
+//         if (queue.length === 0) {
+//             queue.push(i);
+//         } else {
+//             while (queue.length && array[queue.at(-1)] > num) queue.pop();
+//             while (queue.length && array[queue.at(0)] <= num - k) {
+//                 res = Math.min(res, i - queue.at(0));
+//                 queue.shift();
+//             }
+//             queue.push(i);
+//         }
+//     }
     
     return res === Infinity ? -1 : res;
 };
