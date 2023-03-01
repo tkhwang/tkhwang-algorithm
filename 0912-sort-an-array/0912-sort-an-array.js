25f91db93dc9c2ae80dfa44a1e2eb7eee9335083
tkhwang <tkhwang@gmail.com>
2023-03-01 09:49:17 +0900

    Time: 401 ms (54.79%), Space: 67.2 MB (19.90%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -8,33 +8,31 @@ var sortArray = function(nums) {
         const N1 = arr1.length;
         const N2 = arr2.length;
         
-        const res = Array(N1 + N2).fill(0);
+        const res = []
         
         let i1 = 0;
         let i2 = 0;
-        let i = 0;
         
         while (i1 < N1 && i2 < N2) {
             if (arr1[i1] <= arr2[i2]) {
-                res[i] = arr1[i1];
+                res.push(arr1[i1]);
                 i1 += 1;
             } else {
-                res[i] = arr2[i2];
+                res.push(arr2[i2]);
                 i2 += 1;
             }
-            i += 1;
         }
         
-        while (i1 < N1) {
-            res[i] = arr1[i1];
-            i += 1;
-            i1 += 1;
-        }
-        while (i2 < N2) {
-            res[i] = arr2[i2];
-            i += 1;
-            i2 += 1;
-        }
+        // while (i1 < N1) {
+        //     res[i] = arr1[i1];
+        //     i += 1;
+        //     i1 += 1;
+        // }
+        // while (i2 < N2) {
+        //     res[i] = arr2[i2];
+        //     i += 1;
+        //     i2 += 1;
+        // }
         
         // return res;
         return [
