f4b81b8a83b8b6e8204aa68fce7f5d464ca2e11c
tkhwang <tkhwang@gmail.com>
2023-03-02 13:28:48 +0900

    Time: 68 ms (74.68%), Space: 46.6 MB (5.20%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    const N = nums.length;
+    
+    const MIN = 0;
+    const MAX = 1;
+    
+    const dp = Array(N).fill(null).map((_) => Array(2).fill(0));
+    dp[0][MIN] = nums[0];
+    dp[0][MAX] = nums[0];
+    
+    let max = nums[0];
+    
+    for (let i = 1; i < N; i += 1) {
+        dp[i][MIN] = Math.min(
+            dp[i-1][MIN] * nums[i],
+            dp[i-1][MAX] * nums[i],
+            nums[i]
+        )
+        dp[i][MAX] = Math.max(
+            dp[i-1][MIN] * nums[i],
+            dp[i-1][MAX] * nums[i],
+            nums[i]
+        )
+        max = Math.max(max, dp[i][MAX])
+    }
+    
+    return max;
+};
