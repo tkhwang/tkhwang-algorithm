fafcdc67359e5c7a5ac8cb79576b8735527202d4
tkhwang <tkhwang@gmail.com>
2023-03-02 12:31:21 +0900

    Time: 73 ms (65.05%), Space: 44.2 MB (50.52%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.js
@@ -0,0 +1,34 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+    const N = chars.length;
+    
+    let prv = -1;
+    let prvIndex = -1;
+    let count = 0;
+    
+    for (let i = 0; i < N + 1; i += 1) {
+        const cur = chars[i];
+        
+        if (prv === cur) {
+            count += 1;
+        } else {
+            // prvIndex, [prvIndex + 1, ..., i - 1], i
+            if (prv !== -1) {
+                chars.splice(prvIndex + 1, (i-1) - (prvIndex + 1) + 1);
+                if (count > 1) {
+                    const willAdd = String(count).split("");
+                    chars.splice(prvIndex + 1, 0, ...willAdd);
+                    i = prvIndex + 1 + willAdd.length;
+                }
+            }
+            //
+            count = 1;
+            prvIndex = i;
+        }
+        prv = cur;
+    }
+    return chars.length;
+};
