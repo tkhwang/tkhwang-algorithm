8482712070c7c5c5b0ec5968625498201f59b93c
tkhwang <tkhwang@gmail.com>
2023-03-03 10:43:11 +0900

    Time: 75 ms (24.73%), Space: 43.9 MB (7.92%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,13 +4,34 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
+    const ch2index = (ch, base = 'a') => ch.charCodeAt(0) - base.charCodeAt(0);
+    const index2ch = (index, base = 'a') => String.fromCharCode(index + base.charCodeAt(0));
+
     const N = haystack.length;
-    const NN = needle.length;
+    const NT = needle.length;
     
-    for (let i = 0; i < N; i += 1) {
-        const word = haystack.slice(i, i + NN);
-        if (word === needle) return i;
+    const queue = new Queue();
+    let target = 0;
+    for (let i = NT - 1; i >= 0; i -=1) {
+        target = target * 10 + ch2index(needle[i]);
     }
     
-    return -1;
+    let first = -1;
+    let hash = 0;
+    for (let i = N - 1; i >= 0; i -= 1) {
+        const cur = haystack[i];
+        queue.enqueue(cur);
+        hash = 10 * hash + ch2index(cur);
+        
+        if (queue.size() < NT) continue;
+        
+        if (queue.size() > NT) {
+            const oldest = queue.dequeue();
+            hash -= ch2index(oldest) * 10 ** queue.size();
+        }
+        
+        if (hash === target) first = i;
+    }
+    
+    return first;
 };
