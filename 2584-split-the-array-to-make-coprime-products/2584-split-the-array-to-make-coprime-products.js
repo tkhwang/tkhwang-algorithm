f607bd2e19c269887b7b36aba8f2c9baefd43822
tkhwang <tkhwang@gmail.com>
2023-03-06 16:18:43 +0900

    Time: 537 ms (100.00%), Space: 56.3 MB (100.00%) - LeetHub

---
+++ 2584-split-the-array-to-make-coprime-products/2584-split-the-array-to-make-coprime-products.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findValidSplit = function(nums) {
+    const N = nums.length;
+    
+    const primeFactors = (n) => {
+      let counts = {}
+      for (let x = 2; x * x <= n; x++) {
+        while (n % x === 0) {
+          counts[x] = (counts[x] || 0) + 1
+          n /= x
+        }
+      }
+      if (n > 1) counts[n] = (counts[n] || 0) + 1
+      return counts
+    }
+    
+    const farIndex = {};
+    for (let i = N - 1; i >= 0; i -= 1) {
+        const cur = nums[i];
+        const primeObj = primeFactors(cur);
+        for (const prime in primeObj) {
+            if (farIndex[prime] !== undefined) continue;
+            farIndex[prime] = i;
+        }
+    }
+    
+    console.log(farIndex);
+    let max = 0;
+    let i = 0;
+    
+    while (i <= max) {
+        const cur = nums[i];
+        const primeObj = primeFactors(cur);
+        for (const prime in primeObj) {
+             max = Math.max(max, farIndex[prime]);
+        }
+        i += 1;
+    }
+    return max === N - 1 ? -1 : max;
+};
