6664e0670e08f3201ea031c9b1f90f55826176f4
tkhwang <tkhwang@gmail.com>
2023-03-07 10:12:10 +0900

    Time: 10154 ms (5.05%), Space: 75.7 MB (5.96%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+var getPermutation = function(n, k) {
+    const nums = Array(n).fill(null).map((v,i) => i+1);
+    
+    const generatePermutations = (array, length = array.length) => {
+      const res = []
+
+      const dfs = (cur, remain) => {
+        if (cur.length === length) {
+          res.push(cur.join(""));
+          return
+        }
+
+        for (let i = 0; i < remain.length; i += 1) {
+          cur.push(remain[i])
+          dfs(cur, [...remain.slice(0, i), ...remain.slice(i + 1)])
+          cur.pop()
+        }
+      }
+
+      dfs([], array)
+
+      return res
+    }
+
+    const permutations = generatePermutations(nums);
+    
+    return permutations[k-1];
+    
+};
