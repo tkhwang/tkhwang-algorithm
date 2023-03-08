bc17d87a508d02ac17aeb59c14aaf9cb94c2abdb
tkhwang <tkhwang@gmail.com>
2023-03-08 13:28:24 +0900

    Time: 947 ms (45.08%), Space: 0B (100.00%) - LeetHub

---
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+        SELECT
+            customer_number
+        FROM Orders
+        GROUP BY customer_number
+        ORDER BY count(*) DESC
+        LIMIT 1
+
