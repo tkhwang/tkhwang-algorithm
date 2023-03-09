803f6f85a19aab884c3dfa09c62758aaacc2095e
tkhwang <tkhwang@gmail.com>
2023-03-09 12:10:16 +0900

    Time: 199 ms (11.41%), Space: 49.7 MB (6.71%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthUglyNumber = function(n) {
+    const minHeap = new MinPriorityQueue({ compare: (a,b) => a - b });
+    minHeap.enqueue(1);
+    
+    const res = [];
+    const set = new Set();
+    
+    while (res.length < n + 2) {
+        const min = minHeap.dequeue();
+        if (set.has(min)) continue;
+        
+        res.push(min);
+        set.add(min);
+        
+        minHeap.enqueue(min * 2);
+        minHeap.enqueue(min * 3);
+        minHeap.enqueue(min * 5);
+    }
+    
+    return res[n - 1];
+};
