2a3e27cb4c7fe257bcb266e5733e1ec42db2327e
tkhwang <tkhwang@gmail.com>
2023-03-10 18:52:01 +0900

    Time: 100 ms (16.57%), Space: 54.6 MB (5.76%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -5,11 +5,15 @@
 var maxSubArray = function(nums) {
     const N = nums.length;
     
-    let maxest = nums[0];
+    // dp[i]: ending i. max
+    const dp = Array(N).fill(-Infinity);
+    dp[0] = nums[0];
+    
     let max = nums[0];
     for (let i = 1; i < N; i += 1) {
-        max = Math.max(max + nums[i], nums[i]);
-        maxest = Math.max(maxest, max);
+        dp[i] = Math.max(nums[i], dp[i-1] + nums[i]);
+        if (max < dp[i]) max = dp[i];
     }
-    return maxest;
+    
+    return max;
 };
