6f4fa04b9f7135dc3df60c2a3560b6b6245a58dc
tkhwang <tkhwang@gmail.com>
2023-03-13 13:46:54 +0900

    Time: 77 ms (56.67%), Space: 44.1 MB (70.00%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.js
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.js
@@ -4,9 +4,6 @@
  */
 var longestRepeatingSubstring = function(s) {
     const N = s.length;
-
-    const ch2index = (ch, base = 'a') => ch.charCodeAt(0) - base.charCodeAt(0);
-    const index2ch = (index, base = 'a') => String.fromCharCode(index + base.charCodeAt(0));
     
     let left = 0;
     let right =  N - 1;
@@ -15,7 +12,6 @@ var longestRepeatingSubstring = function(s) {
         const set = new Set();
         for (let i = 0; i <= N - size; i += 1) {
             const sub = s.slice(i, i + size)
-            console.log(size, sub)
             if (set.has(sub)) return true;
             set.add(sub);
         }
@@ -34,28 +30,4 @@ var longestRepeatingSubstring = function(s) {
     return right;
 };
 
-//     const isOK = (size) => {
-//         let hash = 0;
-//         const set = new Set();
-//         const queue = new Queue();
-        
-//         for (let i = N - 1; i >= 0; i -= 1) {
-//             const cur = s[i];
-            
-//             hash *= 10;
-//             hash += ch2index(cur);
-//             queue.enqueue(ch2index(cur));
-            
-//             if (queue.size() < size) continue;
-            
-//             if (queue.size() > size) {
-//                 const oldest = queue.dequeue();
-//                 hash -= oldest * 10 ** queue.size();
-//             }
-            
-//             if (set.has(hash)) return true;
-//             set.add(hash);
-//         }
-//         return false;
-//     }
     
