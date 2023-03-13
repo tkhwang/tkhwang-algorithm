5e27e4e0fca327aed4a9cbae1dd2760a522f25a4
tkhwang <tkhwang@gmail.com>
2023-03-13 11:51:35 +0900

    Time: 78 ms (99.64%), Space: 54.5 MB (61.43%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -5,23 +5,18 @@
  */
 var findMaxAverage = function(nums, k) {
     const N = nums.length;
-    const queue = new Queue();
+
     let sum = 0;
-    let max = -Infinity;
+    for (let i = 0; i < k; i += 1) {
+        sum += nums[i];
+    }
+
+    let max = sum;
     
-    for (const num of nums) {
-        queue.enqueue(num);
-        sum += num;
-        if (queue.size() < k) continue;
-        if (queue.size() > k) {
-            const oldest = queue.dequeue();
-            sum -= oldest;
-        }
-        if (queue.size() === k) {
-            const avg = sum / k;
-            if (max < avg) max = avg;
-        }
+    for (let i = k; i < N; i += 1) {
+        sum += nums[i] - nums[i-k];
+        if (max < sum) max = sum;
     }
     
-    return max;
+    return max / k;
 };
