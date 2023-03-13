6b5a760eda8afec23e2a17c8263cf8f5b8500d1f
tkhwang <tkhwang@gmail.com>
2023-03-13 20:29:40 +0900

    Time: 8001 ms (30.00%), Space: 66.8 MB (80.00%) - LeetHub

---
+++ 1044-longest-duplicate-substring/1044-longest-duplicate-substring.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestDupSubstring = function(s) {
+    const N = s.length;
+    
+    let left = 1;
+    let right = N - 1;
+    
+    const isOK = (size) => {
+        const set = new Set();
+        
+        for (let i = 0; i <= N - size; i += 1) {
+            let sub = s.slice(i, i + size);
+            if (set.has(sub)) return sub;
+            set.add(sub);
+        }
+        return ""
+    }
+    
+    let res = "";
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        const result = isOK(mid);
+
+        console.log(mid, result)
+        
+        if (result) {
+            res = result;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return res;
+};
