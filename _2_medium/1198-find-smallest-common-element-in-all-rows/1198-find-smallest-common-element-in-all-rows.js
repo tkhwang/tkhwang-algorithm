3d6239b8c0ec4678058154f9659e5f1eff6ce9f0
tkhwang <tkhwang@gmail.com>
2023-03-15 14:00:55 +0900

    chore(leetcode): :bookmark: re-arrange by difficulty

--- _2_medium/1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.js
+++ _2_medium/1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.js
@@ -3,17 +3,35 @@
  * @return {number}
  */
 var smallestCommonElement = function(mat) {
-    const N = mat.length;
-    const freq = {};
+    const [ ROWS, COLS ] = [ mat.length, mat[0].length ];
     
-    for (const arr of mat) {
-        for (const n of arr) {
-            freq[n] = (freq[n] || 0) + 1;
+    const bisect = (array, target) => {
+        let left = 0;
+        let right = array.length - 1;
+        
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            
+            if (array[mid] === target) return mid;
+            else if (array[mid] < target) {
+                left = mid + 1;
+            } else if (array[mid] > target) {
+                right = mid - 1;
+            }
         }
+        return -1;
     }
     
-    for (const key in freq) {
-        if (freq[key] === N) return key
+    for (const num of mat[0]) {
+        let isMatched = true;
+        for (const array of mat.slice(1)) {
+            const index = bisect(array, num);
+            if (index === -1) {
+                isMatched = false;
+                break;
+            }
+        }
+        if (isMatched) return num;
     }
     return -1;
 };
