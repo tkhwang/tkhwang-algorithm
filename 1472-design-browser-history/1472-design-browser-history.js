ade3d165a1ba22f7962d1e2a48245f44430dd6fe
tkhwang <tkhwang@gmail.com>
2023-03-18 16:15:09 +0900

    Time: 195 ms (33.23%), Space: 52.8 MB (7.49%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.js
@@ -0,0 +1,52 @@
+/**
+ * @param {string} homepage
+ */
+var BrowserHistory = function(homepage) {
+    this.history = [];
+    this.future = [];
+    this.current = homepage;
+};
+
+/** 
+ * @param {string} url
+ * @return {void}
+ */
+BrowserHistory.prototype.visit = function(url) {
+    this.history.push(this.current);
+    this.current = url;
+    this.future.length = 0;
+};
+
+/** 
+ * @param {number} steps
+ * @return {string}
+ */
+BrowserHistory.prototype.back = function(steps) {
+    while (steps > 0 && this.history.length > 0) {
+        this.future.push(this.current);
+        this.current = this.history.pop();
+        steps -= 1;
+    }
+    return this.current;
+};
+
+/** 
+ * @param {number} steps
+ * @return {string}
+ */
+BrowserHistory.prototype.forward = function(steps) {
+    while (steps > 0 && this.future.length > 0) {
+        this.history.push(this.current);
+        this.current = this.future.pop();
+        steps -= 1;
+    }
+    return this.current;
+};
+
+/** 
+ * Your BrowserHistory object will be instantiated and called as such:
+ * var obj = new BrowserHistory(homepage)
+ * obj.visit(url)
+ * var param_2 = obj.back(steps)
+ * var param_3 = obj.forward(steps)
+ */
