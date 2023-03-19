0df32050377bbfef27ac52636a4614ed82e5b5dd
tkhwang <tkhwang@gmail.com>
2023-03-19 16:12:23 +0900

    Time: 440 ms (100.00%), Space: 75.4 MB (100.00%) - LeetHub

---
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} value
+ * @return {number}
+ */
+var findSmallestInteger = function(nums, value) {
+    const N = nums.length;
+
+    const obj = {};
+    const cache = {};
+    
+    const conv = (num) => {
+        if (num >= 0) return num % value;
+        return Math.ceil(-num/value)*value + num;
+    }
+    
+    for (const num of nums) {
+        const key = conv(num);
+        obj[key] = (obj[key] || 0) + 1;
+    }
+    
+    console.log(obj);
+    
+    let target = 0;
+    while (true) {
+        const key = target % value;
+        if (obj[key] === undefined || obj[key] === 0) return target;
+        obj[key] -= 1;
+        if (obj[key] === 0) delete obj[key];
+        target += 1;
+    }
+};
