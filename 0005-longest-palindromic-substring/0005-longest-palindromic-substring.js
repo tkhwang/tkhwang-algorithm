0d5a5e2da646dc49428b414294094b7d2bd3760e
tkhwang <tkhwang@gmail.com>
2023-04-05 14:33:27 +0900

    Time: 105 ms (61.56%), Space: 49.9 MB (17.75%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -8,11 +8,10 @@ var longestPalindrome = function(s) {
     let maxValue = [];
     let max = 0;
     
-    let left = -1;
-    let right = -1;
-    for (let i = 0; i < N; i += 1) {
-        // odd
-        [ left, right ] = [ i, i ]
+    
+    const expandArroundCorner = (left, right) => {
+        let max = 0;
+        let maxValue = [];
         while (0 <= left && right < N && s[left] === s[right]) {
             if (max < right - left + 1) {
                 max = right - left + 1;
@@ -21,16 +20,22 @@ var longestPalindrome = function(s) {
             left -= 1;
             right += 1;
         }
+        return [ max, maxValue ];
+    }
+    
+    for (let i = 0; i < N; i += 1) {
+        // odd
+        const [ oddMax, oddMaxValue ] = expandArroundCorner(i, i);
+        if (max < oddMax) {
+            max = oddMax;
+            maxValue = oddMaxValue;
+        }
         
         // even
-        [ left, right ] = [ i, i + 1 ];
-        while (0 <= left && right < N && s[left] === s[right]) {
-            if (max < right - left + 1) {
-                maxValue = [ left, right ];
-                max = right - left + 1;
-            }
-            left -= 1;
-            right += 1;
+        const [ evenMax, evenMaxValue ] = expandArroundCorner(i, i + 1);
+        if (max < evenMax) {
+            max = evenMax;
+            maxValue = evenMaxValue;
         }
     }
     
