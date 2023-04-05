0195fd59a7f5e3aef8bed0e78998d256fb065ec6
tkhwang <tkhwang@gmail.com>
2023-04-05 11:44:59 +0900

    Time: 175 ms (5.00%), Space: 72.5 MB (5.00%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimizeArrayValue = function(_nums) {
+    const N = _nums.length;
+    
+    /*
+              left, right
+               |
+     */
+    
+    const isOK = (max) => {
+        const nums = [..._nums];
+        if (nums[0] > max) return false;
+        
+        for (let i = N - 1; i >= 1; i -= 1) {
+            let prv = nums[i-1];
+            let cur = nums[i];
+            
+            if (cur < max) continue;
+            
+            const diff = cur - max;
+            nums[i-1] += diff;
+            nums[i] -= diff;
+        }
+        
+        return nums[0] <= max;
+    }
+    
+    let left = Math.min(..._nums);
+    let right = 10 ** 9;
+    
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        
+        if (isOK(mid)) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+};
