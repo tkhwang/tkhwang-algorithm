92de730f41df00b3328b49f0325410ad11e4e20c
tkhwang <tkhwang@gmail.com>
2023-04-05 15:00:31 +0900

    Time: 167 ms (36.21%), Space: 49.9 MB (20.96%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    const cache = {};
+    
+    const dp = (remain) => {
+        if (remain < 0) return -1;
+        if (remain === 0) return 0;
+        if (cache[remain] !== undefined) return cache[remain];
+        
+        let min = Infinity;
+        for (const coin of coins) {
+            if (remain - coin < 0) continue;
+            
+            const res = dp(remain-coin);
+            if (res === -1) continue;
+            min = Math.min(min, 1 + res);
+        }
+        min = min === Infinity ? -1 : min;
+        return cache[remain] = min;
+    }
+    
+    return dp(amount);
+};
