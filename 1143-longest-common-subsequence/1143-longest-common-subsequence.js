8bef1f3c09c9e541cb3a6ed80f5e49edce7882e5
tkhwang <tkhwang@gmail.com>
2023-04-05 15:44:43 +0900

    Time: 118 ms (75.75%), Space: 69.9 MB (25.76%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -11,19 +11,16 @@ var longestCommonSubsequence = function(text1, text2) {
     
     const dp = Array(N1 + 1).fill(null).map((_) => Array(N2 + 1).fill(0));
     
-    let max = 0;
     for (let i = 1; i <= N1; i += 1) {
         for (let j = 1; j <= N2; j += 1) {
             if (text1[i-1] === text2[j-1]) {
                 dp[i][j] = dp[i-1][j-1] + 1
             } else {
                 dp[i][j] = Math.max(
-                    dp[i][j],
                     dp[i-1][j],
                     dp[i][j-1]
                 )
             }
-            if (max < dp[i][j]) max = dp[i][j];
         }
     }
     return dp[N1][N2];
