d3f1d1bdc35968e6da4e1172afad689578df8f0a
tkhwang <tkhwang@gmail.com>
2023-04-11 20:27:44 +0900

    Time: 586 ms (24.14%), Space: 72.4 MB (24.14%) - LeetHub

---
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var isPossibleDivide = function(nums, k) {
+    nums.sort((a,b) => a - b);
+    const map = new Map();
+    for (const num of nums) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
+    
+    const { fist } = map;
+    
+    while (map.size > 0) {
+        console.log(map)
+        
+        let [ cur, _ ] = map.entries().next().value;
+        let remain = k;
+        while (remain > 0) {
+            if (map.get(cur) === undefined) return false;
+            map.set(cur, map.get(cur) - 1);
+            if (map.get(cur) === 0) map.delete(cur);
+            cur += 1;
+            remain -= 1;
+        }
+    }
+    return true;
+};
