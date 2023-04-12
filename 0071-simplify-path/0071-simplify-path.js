d043c332714c3ae35b357bd85a84f21d2f37e417
tkhwang <tkhwang@gmail.com>
2023-04-12 10:28:18 +0900

    Time: 76 ms (28.77%), Space: 44.2 MB (51.58%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.js
+++ 0071-simplify-path/0071-simplify-path.js
@@ -6,16 +6,12 @@ var simplifyPath = function(path) {
     path = path.replaceAll("//", "/");
 
     const paths = path.split("/");
-    
-    console.log(paths);
-    
     const stack = [];
     
     for (const path of paths) {
         if (!path) continue;
         
-        console.log(stack, path);
-        
+
         if (path === "..") {
             if (stack.length > 0) stack.pop();
             continue;
