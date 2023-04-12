bc0fc7c1512b31bae55bb54fbe8e0b0b70fd7f26
tkhwang <tkhwang@gmail.com>
2023-04-12 11:12:11 +0900

    Time: 137 ms (25.49%), Space: 57.2 MB (5.88%) - LeetHub

---
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findPairs = function(nums, k) {
+    nums.sort((a,b) => a - b);
+    
+    const obj = {};
+    const set = new Set();
+    for (const num of nums) {
+        // check
+        if (obj[num - k] !== undefined) {
+            for (const j of obj[num - k]) {
+                const pairs = [ num, j ].sort();
+                const key = pairs.join(":");
+                if (!set.has(key)) set.add(key);
+            }
+        }
+        // save
+        if (obj[num] === undefined) obj[num] = [];
+        obj[num].push(num);
+    }
+    
+    console.log(set);
+    
+    return set.size;
+};
