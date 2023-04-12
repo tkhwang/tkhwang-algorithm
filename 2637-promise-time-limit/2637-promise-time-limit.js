0be618d95a377648c407b678866cd1f0d39610c4
tkhwang <tkhwang@gmail.com>
2023-04-12 10:51:30 +0900

    Time: 60 ms (100.00%), Space: 42.1 MB (100.00%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+        const org = fn(...args);
+        const timeoutPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject("Time Limit Exceeded")
+            }, t)
+        })
+        
+        return Promise.race([
+            org,
+            timeoutPromise
+        ])
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
