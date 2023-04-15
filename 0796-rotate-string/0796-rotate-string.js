74cf9ea686a6056bf2f7867e469a17705053710f
tkhwang <tkhwang@gmail.com>
2023-04-15 16:20:10 +0900

    Time: 77 ms (5.43%), Space: 44.2 MB (5.78%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function(_s, goal) {
+    const s = _s.split("");
+    
+    const set = new Set();
+
+    const shift = (array) => {
+        array.push(array.shift());
+    }
+    
+    let cur = s.join("");
+    while (!set.has(cur)) {
+        set.add(cur);
+        console.log(cur);
+        if (cur === goal) return true;
+        shift(s);
+        cur = s.join("");
+    }
+    return false;
+};
