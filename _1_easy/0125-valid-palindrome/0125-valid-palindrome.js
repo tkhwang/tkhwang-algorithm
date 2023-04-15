6e6ad5841d35ea861f8cba117796abad573ce94d
tkhwang <tkhwang@gmail.com>
2023-04-15 11:57:48 +0900

    chore(leetcode): :bookmark: re-arrange by difficulty

--- _1_easy/0125-valid-palindrome/0125-valid-palindrome.js
+++ _1_easy/0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,7 +3,26 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    const converted = s.replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
+    const N = s.length;
     
-    return converted === converted.split("").reverse().join("");
+    let str = "";
+    
+    for (const ch of s) {
+        if (/[a-zA-Z0-9]/.test(ch)) {
+            str += ch.toLowerCase();
+        } else {
+            continue;
+        }
+    }
+    
+    let left = 0;
+    let right = str.length - 1;
+    
+    while (left < right) {
+        if (str[left] !== str[right]) return false;
+        left += 1;
+        right -= 1;
+    }
+    
+    return true;
 };
