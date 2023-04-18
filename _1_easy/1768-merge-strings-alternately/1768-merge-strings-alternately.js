7218f1e94f3ccd7abb266b264a30e6c5bd933a09
tkhwang <tkhwang@gmail.com>
2023-04-18 20:10:11 +0900

    chore(leetcode): :bookmark: re-arrange by difficulty

--- _1_easy/1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ _1_easy/1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -4,27 +4,12 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    let res = ""
+    const N = Math.max(word1.length, word2.length);
     
-    const N1 = word1.length;
-    const N2 = word2.length;
-    
-    let n1 = 0;
-    let n2 = 0;
-    
-    while (n1 < N1 && n2 < N2) {
-        res += word1[n1] + word2[n2];
-        n1 += 1;
-        n2 += 1;
-    }
-
-    while (n1 < N1) {
-        res += word1[n1];
-        n1 += 1;
-    }
-    while (n2 < N2) {
-        res += word2[n2];
-        n2 += 1;
+    let res = "";
+    for (let i = 0; i < N; i += 1) {
+        if (word1[i]) res += word1[i];
+        if (word2[i]) res += word2[i];
     }
     return res;
 };
