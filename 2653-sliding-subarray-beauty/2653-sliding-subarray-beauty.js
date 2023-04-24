d6beec9c3f13e5da6d57ec2bcc1955db10317277
tkhwang <tkhwang@gmail.com>
2023-04-24 15:06:05 +0900

    Time: 2727 ms (100.00%), Space: 78.1 MB (100.00%) - LeetHub

--- 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.js
+++ 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.js
@@ -6,16 +6,16 @@
  */
 var getSubarrayBeauty = function(nums, k, x) {
     const N = nums.length;
-
-    const freq = {};
-    const res = [];
     
-    const findXthLargest = (freq, x) => {
+    const res = [];
+    const freq = {};
+    
+    const findXthSmallest = (freq, x) => {
         let count = 0;
         for (let i = -50; i < 0; i += 1) {
             count += freq[i] || 0;
             if (count >= x) return i;
-        }    
+        }
         return 0;
     }
     
@@ -27,7 +27,12 @@ var getSubarrayBeauty = function(nums, k, x) {
     for (let i = k - 1; i < N; i += 1) {
         const cur = nums[i];
         freq[cur] = (freq[cur] || 0) + 1;
-        res.push(findXthLargest(freq, x));
+        
+        const value = findXthSmallest(freq, x);
+        res.push(value);
+        
+        //                   |-----------------|
+        //    i - (k - 1) , ... , i-1,  i, i +1
         const oldest = nums[i-k+1];
         freq[oldest] -= 1;
     }
