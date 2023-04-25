78378c0cd1e1a0b9a7dc2215c4b4a2f271c8b97c
tkhwang <tkhwang@gmail.com>
2023-04-25 21:42:21 +0900

    chore(leetcode): :bookmark: re-arrange by difficulty

--- _3_hard/0239-sliding-window-maximum/0239-sliding-window-maximum.js
+++ _3_hard/0239-sliding-window-maximum/0239-sliding-window-maximum.js
@@ -5,28 +5,25 @@
  */
 var maxSlidingWindow = function(nums, k) {
     const N = nums.length;
+    
     const queue = [];
     const res = [];
     
     let left = 0;
     for (let right = 0; right < N; right += 1) {
         const cur = nums[right];
-        
-        while (queue.length && nums[queue.at(-1)] < cur) {
-            queue.pop();
-        }
-        queue.push(right);
+        while (queue.length && queue.at(-1) < cur) queue.pop();
+        queue.push(cur);
         
         if (right - left + 1 > k) {
             const leftNum = nums[left];
-            if (leftNum === nums[queue.at(0)]) {
-                queue.shift();
-            }
+            if (queue.at(0) === leftNum) queue.shift();
             left += 1;
         }
         
-        if (right >= k - 1) res.push(nums[queue.at(0)]);
+        if (right >= k - 1) {
+            res.push(queue.at(0));
+        }
     }
-    
     return res;
 };
