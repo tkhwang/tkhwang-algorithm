5616da94ade09ef3a53ab2b69041bc3826b887c7
tkhwang <tkhwang@gmail.com>
2023-04-26 13:58:22 +0900

    Time: 1563 ms (21.74%), Space: 49 MB (7.45%) - LeetHub

---
+++ 0611-valid-triangle-number/0611-valid-triangle-number.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var triangleNumber = function(nums) {
+    const bisectLeft = (array, target, left = 0, right = array.length) => {
+      // [left, right) half inclusive range
+
+      while (left < right) {
+        const mid = Math.floor((left + right) / 2)
+
+        // left-most
+        if (array[mid] >= target) {
+          right = mid
+        } else {
+          left = mid + 1
+        }
+      }
+      return left
+    }
+
+    const N = nums.length;
+    
+    nums.sort((a,b) => a - b);
+    
+    let sum = 0;
+    
+    for (let i = 0; i < N - 2; i += 1) {
+        for (let j = i + 1; j < N - 1; j += 1) {
+            const index = bisectLeft(nums.slice(j+1), nums[i] + nums[j]);
+            sum += index;
+        }
+    }
+    return sum;
+};
