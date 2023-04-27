cff4b2a8840823393eabe1b5abef99306aeba34c
tkhwang <tkhwang@gmail.com>
2023-04-27 16:29:05 +0900

    Time: 108 ms (11.11%), Space: 42.3 MB (66.67%) - LeetHub

--- 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.js
+++ 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.js
@@ -10,7 +10,9 @@ var subarrayLCM = function(nums, k) {
     const N = nums.length;
     let res = 0;
     for (let i = 0; i < N; i += 1) {
-        for (let j = i, cur = 1; j < N && k % nums[j] === 0; j += 1) {
+        let cur = 1;
+        for (let j = i; j < N; j += 1) {
+            if (k % nums[j] !== 0) break;
             cur = getLCM(cur, nums[j]);
             res += (cur === k ? 1 : 0)
         }
