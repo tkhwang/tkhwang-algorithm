25be57663436f79c3b6389465ad57adc19880825
tkhwang <tkhwang@gmail.com>
2023-04-29 15:23:40 +0900

    chore(leetcode): :bookmark: re-arrange by difficulty

--- _2_medium/0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ _2_medium/0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -5,13 +5,12 @@
 var maxProduct = function(nums) {
     const N = nums.length;
     
+    const dp = Array(N).fill(null).map((_) => Array(2).fill(-1));
     const MIN = 0;
     const MAX = 1;
     
-    const dp = Array(N).fill(null).map((_) => Array(2).fill(0));
     dp[0][MIN] = nums[0];
     dp[0][MAX] = nums[0];
-    
     let max = nums[0];
     
     for (let i = 1; i < N; i += 1) {
@@ -25,8 +24,7 @@ var maxProduct = function(nums) {
             dp[i-1][MAX] * nums[i],
             nums[i]
         )
-        max = Math.max(max, dp[i][MAX])
+        if (max < dp[i][MAX]) max = dp[i][MAX];
     }
-    
     return max;
 };
