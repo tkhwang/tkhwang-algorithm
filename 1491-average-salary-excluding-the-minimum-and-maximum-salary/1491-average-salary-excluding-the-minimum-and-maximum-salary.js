b07c84f3bab2ed211d33b12ba3a9cb582ef77097
tkhwang <tkhwang@gmail.com>
2023-05-01 09:01:27 +0900

    Time: 55 ms (72.35%), Space: 42 MB (39.59%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} salary
+ * @return {number}
+ */
+var average = function(salary) {
+    salary.sort((a,b) => a - b);
+    salary.pop();
+    salary.shift();
+    
+    return salary.reduce((a,b) => a + b, 0) / salary.length;
+};
