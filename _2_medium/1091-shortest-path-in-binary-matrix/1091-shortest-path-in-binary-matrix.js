b2d6b99c7f2499efd610a50f07891d8365ebe8d6
tkhwang <tkhwang@gmail.com>
2023-06-01 22:43:17 +0900

    🐳 chore(leetcode): re-arrange by difficulty

--- _2_medium/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
+++ _2_medium/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
@@ -4,42 +4,37 @@
  */
 var shortestPathBinaryMatrix = function(grid) {
     const [ ROWS, COLS ] = [ grid.length, grid[0].length ];
-    const res = [];
-    
-    const isValid = (r, c) => !(r < 0 || r >= ROWS || c < 0 || c >= COLS);
-    const directions = [ [1,0], [-1,0], [0,1], [0,-1], [-1,-1], [1,1], [1,-1], [-1,1]];
-    const genKey = (r, c) => `${r}:${c}`;
-    
-    const bfs = (r, c) => {
-        const queue = [];
-        queue.push([ r, c ]);
-        let steps = 1;
-        
-        while (queue.length) {
-            const len = queue.length;
-            
-            for (let i = 0; i < len; i += 1) {
-                const [ r, c ] = queue.shift();
-                if (r === ROWS -1 && c === COLS - 1) return steps;
-                
-                for (const [ dR, dC ] of directions) {
-                    const newR = r + dR;
-                    const newC = c + dC;
-                    
-                    if (!isValid(newR, newC)) continue;
-                    if (grid[newR][newC] === 1) continue;
-
-                    grid[newR][newC] = 1;
-                    queue.push([ newR, newC ]);
-                }
-            }
-            
-            steps += 1;
-        }
-        return -1;
-    }
     
     if (grid[0][0] === 1) return -1;
     
-    return bfs(0, 0);
+    const directions = [ [1,0], [-1,0], [0, 1], [0, -1], [1, 1], [-1, -1], [1, -1], [-1, 1] ];
+    
+    const seen = Array(ROWS).fill(null).map((_) => Array(COLS).fill(Infinity));
+    let min = Infinity;
+    
+    const isValid = (r, c) => !(r < 0 || r >= ROWS || c < 0 || c >= COLS);
+    
+    const queue = [ [ 0, 0, 1 ] ];
+    while (queue.length) {
+        const len = queue.length;
+        for (let i = 0; i < len; i += 1) {
+            const [ r, c, steps ] = queue.shift();
+            
+            if (r === ROWS - 1 && c == COLS - 1) return steps;
+            
+            for (const [ dR, dC ] of directions) {
+                const newR = r + dR;
+                const newC = c + dC;
+                
+                if (!isValid(newR, newC)) continue;
+                if (grid[newR][newC] === 1) continue;
+                if (seen[newR][newC] <= steps + 1) continue;
+
+                seen[newR][newC] = steps + 1;
+                queue.push([ newR, newC, steps + 1 ]);
+            }
+        }
+    }
+
+    return -1;
 };
