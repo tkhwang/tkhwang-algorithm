50d70a3336ea4a8766ab9b551960caf4e3272dbe
tkhwang <tkhwang@gmail.com>
2023-09-08 04:37:20 +0900

    Time: 57 ms (36.57%), Space: 41.7 MB (89.75%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function(head, left, right) {
+     if (!head || left === right) return head;
+    
+    const dummy = new ListNode(0);
+    dummy.next = head;
+    let prev = dummy;
+    
+    for (let i = 0; i < left - 1; ++i) {
+        prev = prev.next;
+    }
+    
+    let current = prev.next;
+    
+    for (let i = 0; i < right - left; ++i) {
+        const nextNode = current.next;
+        current.next = nextNode.next;
+        nextNode.next = prev.next;
+        prev.next = nextNode;
+    }
+    
+    return dummy.next;
+};
