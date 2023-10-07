425c1a95fe8f209e49a107dc85ac767127a2375a
tkhwang <tkhwang@gmail.com>
2023-10-07 09:03:36 +0900

    Time: 59 ms (27.00%), Space: 42.6 MB (16.00%) - LeetHub

--- 0343-integer-break/0343-integer-break.js
+++ 0343-integer-break/0343-integer-break.js
@@ -3,17 +3,23 @@
  * @return {number}
  */
 var integerBreak = function(n) {
-    if (n === 2) return 1;
-    if (n === 3) return 2;
-
-    let count_of_3s = Math.floor(n / 3);
-    let remainder = n % 3;
-
-    if (remainder === 0) {
-        return 3 ** count_of_3s;
-    } else if (remainder === 1) {
-        return 3 ** (count_of_3s - 1) * 4;
-    } else {
-        return 3 ** count_of_3s * 2;
-    }    
+    let max = -Infinity;
+    
+    const cache = {};
+    
+    const dp = (num) => {
+        if (num === 1) return 1;
+        if (cache[num] !== undefined) return cache[num];
+        
+        let res = num === n ? 0 : num;
+        
+        for (let i = 1; i < num; i += 1) {
+            const local = dp(i) * dp(num-i);
+            res = Math.max(res, local);
+        }
+        
+        return cache[num] = res;
+    }
+    
+    return dp(n)
 };
