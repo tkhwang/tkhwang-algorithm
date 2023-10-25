e059b4d1388e8e2a0126f6129fd003a8ce9e0946
tkhwang <tkhwang@gmail.com>
2023-10-26 07:44:14 +0900

    Time: 54 ms (39.33%), Space: 41.5 MB (74.16%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var kthGrammar = function(n, k) {
+    if (n === 1) return 0;
+    let length = 1 << (n - 2);
+    if (k <= length) return kthGrammar(n - 1, k);
+    else return 1 - kthGrammar(n - 1, k - length);    
+};
