c57a3d85780c3f6906f0b01ab84078b2bebaf5b3
tkhwang <tkhwang@gmail.com>
2023-10-29 08:44:23 +0900

    🐳 chore(leetcode): re-arrange by difficulty

--- _3_hard/1220-count-vowels-permutation/1220-count-vowels-permutation.js
+++ _3_hard/1220-count-vowels-permutation/1220-count-vowels-permutation.js
@@ -2,19 +2,10 @@
  * @param {number} n
  * @return {number}
  */
-var countVowelPermutation = function(n) {
-
-    /*
-        a   e  i  o  u (current)    
-     a     +1
-     e  +1    +1
-     i  +1 +1     +1 +1
-     o        +1     +1
-     u  +1
-  (prv)
-     */
-
-    const MOD = 7 + 10 ** 9;
+var countVowelPermutation = function(N) {
+    const res = [];
+    
+    const vowels = [ 'a', 'e', 'i', 'o', 'u' ];
     
     const A = 0;
     const E = 1;
@@ -22,21 +13,22 @@ var countVowelPermutation = function(n) {
     const O = 3;
     const U = 4;
     
-    // dp[i][character]
-    const dp = Array(n + 1).fill(null).map((_) => Array(5).fill(0));
+    const dp = Array(N + 1).fill(null).map((_) => Array(5).fill(0));
     dp[1][A] = 1;
     dp[1][E] = 1;
     dp[1][I] = 1;
     dp[1][O] = 1;
     dp[1][U] = 1;
     
-    for (let i = 2; i <= n; i += 1) {
-        dp[i][A] = (dp[i-1][E] + dp[i-1][I] +dp[i-1][U]) % MOD;
-        dp[i][E] = (dp[i-1][A] + dp[i-1][I]) % MOD;
-        dp[i][I] = (dp[i-1][E] + dp[i-1][O]) % MOD;
-        dp[i][O] = (dp[i-1][I]) % MOD;
-        dp[i][U] = (dp[i-1][I] + dp[i-1][O]) % MOD;
+    const MOD = 7 + 10 ** 9;
+    
+    for (let i = 2; i <= N; i += 1) {
+        dp[i][A] = ( dp[i-1][E] + dp[i-1][I] + dp[i-1][U] ) % MOD;
+        dp[i][E] = ( dp[i-1][A] + dp[i-1][I] ) % MOD;
+        dp[i][I] = ( dp[i-1][E] + dp[i-1][O] ) % MOD;
+        dp[i][O] = ( dp[i-1][I] ) % MOD;
+        dp[i][U] = ( dp[i-1][I] + dp[i-1][O] ) % MOD;
     }
     
-    return dp[n].reduce((a,b) => a + b, 0) % MOD;
+    return dp[N].reduce((a,b) => a + b, 0) % MOD;
 };
