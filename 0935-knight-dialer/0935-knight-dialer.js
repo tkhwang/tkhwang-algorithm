8b3aaf8d55ae18108d233c73fac6f81893761a92
tkhwang <tkhwang@gmail.com>
2023-11-28 07:22:48 +0900

    Time: 197 ms (64.86%), Space: 56.5 MB (64.86%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var knightDialer = function(n) {
+const moves = {
+    0: [ 4, 6 ],
+    1: [ 6, 8 ],
+    2: [ 7, 9 ],
+    3: [ 4, 8 ],
+    4: [ 0, 3, 9 ],
+    5: [],
+    6: [ 0, 1, 7 ],
+    7: [ 2, 6 ],
+    8: [ 1, 3 ],
+    9: [ 2, 4 ],
+}
+
+    const dp = Array.from({ length: n + 1 }, (_,i) => Array(10).fill(i===1?1:0) )
+    const mod = 10 ** 9 + 7
+    
+    for ( let count = 1; count <= n; count++ )
+        for ( let digit = 0; digit <= 9; digit++ )
+            moves[ digit ].forEach( move => dp[ count ][ digit ] += dp[ count - 1 ][ move ] % mod )
+ 
+    return dp[ n ].reduce( ( a, x ) => a + x, 0 ) % mod
+};
