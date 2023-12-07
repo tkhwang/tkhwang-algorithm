ac46f7371407d5d6a8ff86218395952ec9622d00
tkhwang <tkhwang@gmail.com>
2023-12-08 00:12:05 +0900

    Time: 61 ms (59.72%), Space: 45.9 MB (26.57%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function(num) {
+    const N = num.length;
+    
+    let right = N - 1;
+
+    while (right >= 0 && num[right] % 2 === 0) right -= 1;
+    
+    console.log(right);
+    
+    if (right === -1) return "";
+    
+    return num.slice(0, right + 1);
+    
+};
