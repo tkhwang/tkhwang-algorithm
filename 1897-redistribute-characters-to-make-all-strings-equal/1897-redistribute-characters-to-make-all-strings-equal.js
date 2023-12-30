82ebbbf46e240a29e4ea989235a749d6df967598
tkhwang <tkhwang@gmail.com>
2023-12-30 20:47:14 +0900

    Time: 165 ms (5.63%), Space: 49.5 MB (5.63%) - LeetHub

---
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} words
+ * @return {boolean}
+ */
+var makeEqual = function(words) {
+    const N = words.length;
+    
+    const dp = Array(26).fill(0);
+    
+    for (const word of words) {
+        for (const ch of word) {
+            const index = ch.charCodeAt('0') - 'a'.charCodeAt('0');
+            dp[index] = (dp[index] || 0) + 1
+        }
+    }
+    
+    console.log(dp);
+    
+    for (let i = 0 ; i < 26; i += 1) {
+        if (dp[i] % N !== 0) return false;
+    }
+    
+    return true;
+};
