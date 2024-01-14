d9f4708e2e7cab000e3c8ccea7c4c571580daf52
tkhwang <tkhwang@gmail.com>
2024-01-15 08:25:29 +0900

    Time: 73 ms (80.33%), Space: 47.5 MB (53.72%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var minSteps = function(s, t) {
+    const countS = new Array(26).fill(0);
+    const countT = new Array(26).fill(0);
+
+    for (let i = 0; i < s.length; i++) {
+        countS[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        countT[t.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+    }
+
+    let steps = 0;
+    for (let i = 0; i < 26; i++) {
+        steps += Math.abs(countS[i] - countT[i]);
+    }
+
+    return Math.floor(steps / 2);      
+};
