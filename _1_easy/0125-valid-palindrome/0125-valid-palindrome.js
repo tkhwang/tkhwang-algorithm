d8c41ec548a1518dc359ee89da631b64fcd2b1b1
tkhwang <tkhwang@gmail.com>
2024-03-13 08:26:41 +0900

    🐳 chore(leetcode): re-arrange by difficulty

--- _1_easy/0125-valid-palindrome/0125-valid-palindrome.js
+++ _1_easy/0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,26 +3,7 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    const N = s.length;
+    const str = s.toLowerCase().replace(/[^0-9a-z]/g, "");
     
-    let str = "";
-    
-    for (const ch of s) {
-        if (/[a-zA-Z0-9]/.test(ch)) {
-            str += ch.toLowerCase();
-        } else {
-            continue;
-        }
-    }
-    
-    let left = 0;
-    let right = str.length - 1;
-    
-    while (left < right) {
-        if (str[left] !== str[right]) return false;
-        left += 1;
-        right -= 1;
-    }
-    
-    return true;
+    return str === str.split("").reverse().join("");
 };
--- _2_medium/1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.js
+++ _2_medium/1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.js
@@ -8,12 +8,12 @@ var minimumLength = function(s) {
     let left = 0;
     let right = N - 1;
     let ch;
+    
     while (left < right && s[left] === s[right]) {
         ch = s[left];
         while (left <= right && ch === s[left]) left += 1;
         while (left <= right && ch === s[right]) right -= 1;
     }
     
-    // return s[left] === ch ? 0 : right - left + 1;
     return right - left + 1;
 };
