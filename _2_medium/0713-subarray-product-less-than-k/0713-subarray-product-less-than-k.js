5baf764578c6a3e7dcb9fcd0f1926f360d8eb7cd
tkhwang <tkhwang@gmail.com>
2024-03-29 08:55:59 +0900

    🐳 chore(leetcode): re-arrange by difficulty

--- _2_medium/0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.js
+++ _2_medium/0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.js
@@ -5,19 +5,21 @@
  */
 var numSubarrayProductLessThanK = function(nums, k) {
     const N = nums.length;
-    
+
     if (k <= 1) return 0;
-    
-    let product = 1;
+
     let left = 0;
+    let product = 1;
     let res = 0;
     for (let right = 0; right < N; right += 1) {
         product *= nums[right];
+
         while (product >= k) {
             product /= nums[left];
             left += 1;
         }
         res += right - left + 1;
-    }
+    }    
+
     return res;
 };
