7bd7228310a5d22a65472dfc0b7a9f5df6b762c1
tkhwang <tkhwang@gmail.com>
2024-05-18 08:21:48 +0900

    Time: 80 ms (26.07%), Space: 53.4 MB (78.63%) - LeetHub

---
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {TreeNode}
+ */
+var removeLeafNodes = function(root, target) {
+    if (root.left) {
+        root.left = removeLeafNodes(root.left, target);
+    }
+    if (root.right) {
+        root.right = removeLeafNodes(root.right, target);
+    }
+    return root.left === root.right && root.val === target ? null : root;
+};
