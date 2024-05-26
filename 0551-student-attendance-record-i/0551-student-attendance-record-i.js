6598480049be444d2110b3fce561aa68d92ff81d
tkhwang <tkhwang@gmail.com>
2024-05-26 09:30:29 +0900

    Time: 62 ms (14.46%), Space: 53.5 MB (5.72%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.js
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.js
@@ -15,16 +15,6 @@ var checkRecord = function(s) {
     const hasAbsentOk = absents.length < 2;
     if (!hasAbsentOk) return false;
 
-    for (let right = 0; right < N; right += 1) {
-        const cur = s[right];
-        if (cur !== "L") continue;
-
-        let left = right;
-        while (s[left] === 'L' && left < N) {
-            left += 1;
-        }
-        if (left - right + 1 >= 3 + 1) return false;
-    }
-
-    return true;
+    const hasWrongLate = s.indexOf("LLL") > -1;
+    return !hasWrongLate;
 };
