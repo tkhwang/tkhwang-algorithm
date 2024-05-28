301bdb7ff82b8dda9a2057930d615943c478ccbc
tkhwang <tkhwang@gmail.com>
2024-05-28 22:02:40 +0900

    🐳 chore(leetcode): re-organize by difficulty

--- _2_medium/0131-palindrome-partitioning/0131-palindrome-partitioning.js
+++ _2_medium/0131-palindrome-partitioning/0131-palindrome-partitioning.js
@@ -4,32 +4,33 @@
  */
 var partition = function(s) {
     const N = s.length;
-    const res = [];
     
-    const isPlindrome = (str) => {
+    const isP = (array) => {
         let left = 0;
-        let right = str.length - 1;
+        let right = array.length - 1;
         
-        while (left < right) {
-            if (str[left] !== str[right]) return false;
+        while (left <= right) {
+            if (array[left] !== array[right]) return false;
             left += 1;
             right -= 1;
         }
         return true;
     }
     
+    const res = [];
+    
     const dfs = (cur, index) => {
         if (index >= N) {
             res.push([...cur]);
             return;
-        }    
+        }
         
         for (let i = index; i < N; i += 1) {
-            const candidate = s.slice(index, i + 1);
-            if (isPlindrome(candidate)) {
-                cur.push(candidate);
+            const word = s.slice(index, i + 1);
+            if (isP(word)) {
+                cur.push(word);
                 dfs(cur, i + 1);
-                cur.pop()
+                cur.pop();
             }
         }
     }
--- _3_hard/0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.js
+++ _3_hard/0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.js
@@ -2,44 +2,43 @@
  * @param {string} s
  * @return {string[]}
  */
-var removeInvalidParentheses = function(s) {
-    const N = s.length;
-    
-    const isValid = (str) => {
-        let left = 0;
-        let right = 0;
-        for (const [ i, ch ] of str.split("").entries()) {
-            if (ch === "(") left += 1;
-            if (ch === ")") right += 1;
-            if (left < right) return false;
-        }
-        return left === right;
-    }
-    
-    const res = [];
-    const set = new Set();
-    const dfs = (remain, start) => {
+var removeInvalidParentheses = function (s) {
+  const N = s.length
 
-        if (isValid(remain)) {
-            if (!set.has(remain)) {
-                set.add(remain);
-                res.push(remain);
-            }
-            return;
-        }
-        
-        //   i-1, i, i+1
-        //        
-        for (let i = start; i < remain.length; i += 1) {
-            const cur = remain[i];
-            if (cur === "(" || cur === ")") dfs(remain.slice(0, i) + remain.slice(i+1), i)
-        }
+  const isValid = (str) => {
+    let left = 0
+    let right = 0
+    for (const [i, ch] of str.split("").entries()) {
+      if (ch === "(") left += 1
+      if (ch === ")") right += 1
+      if (left < right) return false
     }
-    
-    dfs(s, 0);
-    
-    res.sort((a,b) => b.length - a.length);
-    const max = res[0].length;
-    
-    return res.filter((v) => v.length === max);
-};
+    return left === right
+  }
+
+  const res = []
+  const set = new Set()
+  const dfs = (remain, start) => {
+    if (isValid(remain)) {
+      if (!set.has(remain)) {
+        set.add(remain)
+        res.push(remain)
+      }
+      return
+    }
+
+    //   i-1, i, i+1
+    //
+    for (let i = start; i < remain.length; i += 1) {
+      const cur = remain[i]
+      if (cur === "(" || cur === ")") dfs(remain.slice(0, i) + remain.slice(i + 1), i)
+    }
+  }
+
+  dfs(s, 0)
+
+  res.sort((a, b) => b.length - a.length)
+  const max = res[0].length
+
+  return res.filter((v) => v.length === max)
+}
