4ca416ce0b74646e85a064b3ef4ab0b666450946
tkhwang <tkhwang@gmail.com>
2024-12-28 08:49:43 +0900

    Time: 3 ms (18.75%), Space: 54.4 MB (81.25%) - LeetHub

---
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} values
+ * @return {number}
+ */
+var maxScoreSightseeingPair = function(values) {
+    const N = values.length;
+    
+    let maxFromLeft = -Infinity;
+    let max = -Infinity;
+    
+    for (let i = 0; i < N; i += 1) {
+        max = Math.max(maxFromLeft + values[i] - i, max)
+        maxFromLeft = Math.max(maxFromLeft, values[i] + i);
+            
+    }
+    return max;    
+};
