922aea53dc730c457b0c3c4c7f511cc0d9fc4029
tkhwang <tkhwang@gmail.com>
2025-09-30 17:45:50 +0900

    Time: 0 ms (100%), Space: 55.4 MB (33.8%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.ts
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.ts
@@ -1,38 +1,39 @@
 class MyQueue {
-    private stackIN: number[] = [];
-    private stackOUT: number[] = [];
+    private stackIN: number[];
+    private stackOUT: number[];
 
-    push(value: number): void {
-        this.stackIN.push(value);
+    constructor() {
+        this.stackIN = [];
+        this.stackOUT = [];
+    }
+ 
+    push(x: number): void {
+        this.stackIN.push(x);
     }
 
-    pop(): number | undefined {
-        if (this.empty()) return -1;
-
+    pop(): number {
         if (this.stackOUT.length === 0) {
             while (this.stackIN.length > 0) {
-                this.stackOUT.push(this.stackIN.pop()!);
+                this.stackOUT.push(this.stackIN.pop())
             }
         }
-        return this.stackOUT.pop();
+        return this.stackOUT.pop() ?? -1;
     }
 
     peek(): number {
-        if (this.empty()) return -1;
-
         if (this.stackOUT.length === 0) {
             while (this.stackIN.length > 0) {
-                this.stackOUT.push(this.stackIN.pop()!);
+                this.stackOUT.push(this.stackIN.pop())
             }
         }
-        return this.stackOUT[this.stackOUT.length - 1];
+                
+        return this.stackOUT.at(-1) ?? -1;
     }
 
     empty(): boolean {
         return this.stackIN.length === 0 && this.stackOUT.length === 0;
     }
 }
-
 
 /**
  * Your MyQueue object will be instantiated and called as such:
