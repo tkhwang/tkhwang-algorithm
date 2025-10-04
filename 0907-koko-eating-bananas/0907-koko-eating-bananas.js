e6467a6ae10bd3fefe4c93f24945074ef04bb1ad
tkhwang <tkhwang@gmail.com>
2025-10-04 15:19:02 +0900

    Time: 40 ms (6.19%), Space: 61.2 MB (5.56%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    piles.sort((a,b) => a - b);
+
+    console.log(piles);
+
+    const isOK = (speed) => {
+        let times = 0;
+        for (const pile of piles) {
+            times += Math.ceil(pile/speed);
+        }
+        return times <= h;
+    }
+
+    let left = 1;
+    let right = 10 ** 9;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (isOK(mid)) right = mid - 1;
+        else left = mid + 1;
+    }
+    return left;
+};
