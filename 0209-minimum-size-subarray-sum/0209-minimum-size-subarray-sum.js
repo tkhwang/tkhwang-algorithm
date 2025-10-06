35578babe6e6bfafb078f1cb2cb10e714dde40ea
tkhwang <tkhwang@gmail.com>
2025-10-06 16:00:34 +0900

    Time: 1 ms (94.57%), Space: 59.9 MB (39.98%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    const N = nums.length;
+
+    let min = Infinity;
+    let sum = 0;
+    let left = 0;
+    for (let right = 0; right < N; right += 1) {
+        const cur = nums[right];
+        sum += cur;
+
+        while (sum >= target) {
+            if (min > right - left + 1) min = right - left + 1;
+
+            const leftNum = nums[left];
+            sum -= leftNum;
+            left += 1;
+        }
+    }
+
+    return min === Infinity ? 0 : min;
+};
