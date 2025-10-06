61e822dbbdb99871daf5d86a72ada4796bfb5e06
tkhwang <tkhwang@gmail.com>
2025-10-06 17:11:59 +0900

    Time: 3 ms (94.59%), Space: 55.9 MB (91.91%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -5,18 +5,17 @@
 var lengthOfLongestSubstring = function(s) {
     const N = s.length;
 
-    const window = {};
+    const window = new Map();
     let max = 0;
 
     let left = 0;
     for (let right = 0; right < N; right += 1) {
         const cur = s[right];
-        window[cur] = (window[cur] || 0) + 1;
+        window.set(cur, (window.get(cur) ?? 0) + 1);
         
-        while (window[cur] > 1) {
+        while (window.get(cur) > 1) {
             const leftNum = s[left];
-            window[leftNum] -= 1;
-            if (window[leftNum] === 0) delete window[leftNum];
+            window.set(leftNum, window.get(leftNum) - 1);
             left += 1;
         }
 
