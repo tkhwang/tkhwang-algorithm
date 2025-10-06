2618761a76cc7202d27772f860c9713a511378a9
tkhwang <tkhwang@gmail.com>
2025-10-06 20:32:30 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    const N = nums.length;
+
+    const minHeap = new MinPriorityQueue({ compare: (a,b) => b - a });
+
+    for (const num of nums) {
+        minHeap.enqueue(num);
+        if (minHeap.size() > k) minHeap.dequeue();
+    }
+
+    return minHeap.front();
+};
