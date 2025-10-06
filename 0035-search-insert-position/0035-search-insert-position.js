10ab5e6ddd34b3c40045858b97aacc69403eb808
tkhwang <tkhwang@gmail.com>
2025-10-06 15:51:27 +0900

    Time: 0 ms (100%), Space: 52.7 MB (97.44%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    const N = nums.length;
+
+    let left = 0;
+    let right = N - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) return mid;
+        else if (nums[mid] < target) {
+            left = mid + 1;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        }
+    }
+    return left;
+};
