7710e3771b6c7dc46f53b5681ce15709c992feb5
tkhwang <tkhwang@gmail.com>
2025-10-10 09:25:02 +0900

    Time: 102 ms (28.57%), Space: 71.4 MB (42.86%) - LeetHub

---
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} energy
+ * @param {number} k
+ * @return {number}
+ */
+var maximumEnergy = function(energy, k) {
+    const N = energy.length;
+
+    const dp = Array(N + 1) .fill(0);
+    dp[N-1] = energy[N-1];
+
+    let max = -Infinity;
+
+    for (let i = N- 1; i >= 0; i -= 1) {
+        if (dp[i+k] !== undefined) {
+            dp[i] = dp[i+k] + energy[i]
+        } else {
+            dp[i] = energy[i]
+        }
+        if (max < dp[i]) max = dp[i];
+    }
+
+    return max;
+};
