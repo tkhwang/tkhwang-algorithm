337df29ea7476514e116adbb293b11d5533e32bf
tkhwang <tkhwang@gmail.com>
2025-10-14 12:58:04 +0900

    Time: 252 ms (39.24%), Space: 154.8 MB (16.84%) - LeetHub

--- 0204-count-primes/0204-count-primes.js
+++ 0204-count-primes/0204-count-primes.js
@@ -2,22 +2,20 @@
  * @param {number} n
  * @return {number}
  */
-var countPrimes = function(N) {
-    const primes = Array(N + 1).fill(true);
-    primes[0] = false;
-    primes[1] = false;
+var countPrimes = function(n) {
+    if (n < 2) return 0;
 
-    for (let i = 2; i < Math.sqrt(N) + 1; i += 1) {
-        if (primes[i]) {
-            for (let j = i * i; j < N; j += i) {
-                primes[j] = false;
-            }
+    const sieve = new Array(n).fill(true);
+    sieve[0] = sieve[1] = false;
+
+    for (let i = 2; i * i <= n; i += 1) {
+        if (!sieve[i]) continue;
+
+        // found prime
+        for (let j = i * i; j < n; j += i) {
+            sieve[j] = false;
         }
     }
-    
-    let count = 0;
-    for (let i = 2; i < N; i += 1) {
-        if (primes[i]) count += 1;
-    }
-    return count;
+
+    return sieve.filter(Boolean).length;
 };
