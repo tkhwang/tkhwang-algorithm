d7b7be62e62f9b1ba321ca3ba7ae53262c501a5c
tkhwang <tkhwang@gmail.com>
2025-10-16 08:22:53 +0900

    Time: 118 ms (18.18%), Space: 83.4 MB (21.21%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.js
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.js
@@ -21,15 +21,17 @@ var maxIncreasingSubarrays = function(nums) {
         return false;
     }
 
+    let result = 1;
     while (left <= right) {
         const mid = Math.floor( (left + right) / 2);
 
         if (isOK(mid)) {
+            result = mid;
             left = mid + 1;            
         } else {
             right = mid - 1;
         }
     }
 
-    return left - 1;
+    return result;
 };
