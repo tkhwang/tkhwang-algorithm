767004e39d168123be1ea9680b799d8c87a623a5
tkhwang <tkhwang@gmail.com>
2025-10-17 12:09:14 +0900

    Time: 144 ms (13.33%), Space: 102.4 MB (43.33%) - LeetHub

---
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums
+ * @param {number[][]} queries
+ * @return {number}
+ */
+var minZeroArray = function(nums, queries) {
+    const N = nums.length;
+
+    // number of query
+    let left = 0;
+    let right = queries.length;
+
+    const isOK = (numberOfQuery) => {
+        const diff = Array(N + 1).fill(0);
+    
+        for (let i = 0; i < numberOfQuery; i += 1) {
+            const [ left, right, value ] = queries[i];
+
+            diff[left] += value;
+            if (right + 1 < N) diff[right + 1] -= value;
+        }
+
+        let pendingDecrements = 0;
+        for (let i = 0; i < N; i += 1) {
+            pendingDecrements += diff[i];
+            if (pendingDecrements < nums[i]) return false;
+        }
+        return true;
+    }
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        // find minimum
+        if (isOK(mid)) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left > queries.length ? -1 : left; 
+};
