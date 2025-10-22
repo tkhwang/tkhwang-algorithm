e3a8253b0c7f609b8ff8eb7acb8bdc7048713979
tkhwang <tkhwang@gmail.com>
2025-10-23 04:36:18 +0900

    Time: 13 ms (91.12%), Space: 74.1 MB (45.2%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const set = new Set();
+
+    for (const num of nums) {
+        if (set.has(num)) return true;
+
+        set.add(num);
+    }
+
+    return false;
+};
