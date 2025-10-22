c39d0c5a6f08f0c5fc366d7365f2270a721d912a
tkhwang <tkhwang@gmail.com>
2025-10-22 13:42:22 +0900

    Time: 1971 ms (8.46%), Space: 60.4 MB (18.51%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -4,27 +4,19 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-    const trie = {};
-
-    let node = trie;
-    for (const ch of word) {
-        if (node[ch] === undefined) node[ch] = {};
-        node = node[ch];
-    }
-    node["word"] = word;
-
-    const directions = [ [1,0], [-1,0], [0, 1], [0, -1] ];
-    const isValid = (r,c) => !(r < 0 || r >= ROWS || c < 0 || c>= COLS );
+    const N = word.length;
+    const [ ROWS, COLS ] = [ board.length, board[0].length ];
+    const directions = [ [ 1, 0], [-1, 0], [0, 1], [0, -1] ];
+    const isValid = (r, c) => !(r < 0 || r >= ROWS || c < 0 || c >= COLS);
     const genKey = (r, c) => `${r}:${c}`;
 
-    const dfs = (r, c, node, seen) => {
-        const ch = board[r][c];
-
-        if (node[ch] === undefined) return false;
-        if (node[ch]["word"]) return true;
+    const backtrack = (r, c, index, seen) => {
+        if (word[index] === undefined) return false;
+        if (board[r][c] !== word[index]) return false;
+        if (index >= N - 1) return true;
 
         let temp = false;
-        for (const [ dR, dC ] of directions) {
+        for (const [ dR, dC ] of directions){
             const newR = r + dR;
             const newC = c + dC;
 
@@ -32,25 +24,20 @@ var exist = function(board, word) {
             if (seen.has(genKey(newR, newC))) continue;
 
             seen.add(genKey(newR, newC));
-            temp = temp || dfs(newR, newC, node[ch], seen);
+            temp = temp || backtrack(newR, newC, index + 1, seen);
             seen.delete(genKey(newR, newC));
         }
         return temp;
     }
 
-
-    // search
-    const [ ROWS, COLS ] = [ board.length, board[0].length ];
-
     for (let r = 0; r < ROWS; r += 1) {
         for (let c = 0; c < COLS; c += 1) {
             const cur = board[r][c];
-            if (trie[cur] === undefined) continue;
+            if (cur !== word[0]) continue;
 
             const seen = new Set();
-            seen.add(genKey(r, c));
-
-            if (dfs(r, c, trie, seen)) return true;
+            seen.add(genKey(r, c))
+            if(backtrack(r, c, 0, seen)) return true;
         }
     }
     return false;
