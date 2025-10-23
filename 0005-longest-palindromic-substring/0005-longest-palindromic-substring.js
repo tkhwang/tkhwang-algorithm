f0b40949dae42c0759276e82da44b14a1c0c1ccf
tkhwang <tkhwang@gmail.com>
2025-10-23 14:40:58 +0900

    Time: 650 ms (8.21%), Space: 93.4 MB (5.06%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -6,36 +6,33 @@ var longestPalindrome = function(s) {
     const N = s.length;
     if (N <= 1) return s;
 
-    const dp = Array(N).fill(null).map(() => Array(N).fill(false));
-
     let start = 0;
-    let maxLen = 1;
+    let maxLength = 1;
+
+    const dp = Array(N).fill(0).map((_) => Array(N).fill(false));
 
     for (let i = 0; i < N; i += 1) {
         dp[i][i] = true;
     }
 
-    for (let len = 2; len <= N; len += 1) {
-        for (let i = 0; i + len - 1 < N; i += 1) {
-            const j = i + len - 1;
+    for (let length = 2; length <= N; length += 1) {
+        for (let i = 0; i <= N - length; i += 1) {
+            // j - i + 1 = length
+            const j = i + length - 1;
+            if (s[i] === s[j]) {
+                if (length === 2) {
+                    dp[i][j] = true;
+                } else {
+                    dp[i][j] = dp[i+1][j-1]
+                }
 
-            if (s[i] !== s[j]) {
-                dp[i][j] = false;
-                continue;
-            }
-
-            if (len <= 3) {
-                dp[i][j] = true;
-            } else {
-                dp[i][j] = dp[i+1][j-1];
-            }
-
-            if (dp[i][j] && len > maxLen) {
-                maxLen = len;
-                start = i;
-            }
+                if (dp[i][j] && length > maxLength) {
+                    start = i;
+                    maxLength = length;
+                }
+            } 
         }
     }
 
-    return s.substring(start, start + maxLen);
+    return s.substring(start, start + maxLength);
 };
