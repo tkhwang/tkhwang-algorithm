1e4ac29ff4ea07085df059ef334217fb20098f38
tkhwang <tkhwang@gmail.com>
2025-10-23 13:39:36 +0900

    Time: 0 ms (100%), Space: 54.4 MB (19.94%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -7,7 +7,7 @@ var rob = function(nums) {
 
     const dp = Array(N + 1).fill(0);
     dp[0] = nums[0];
-    dp[1] = nums[1];
+    dp[1] = Math.max(nums[0], nums[1]);
 
     for (let i = 2; i < N; i += 1) {
         dp[i] = Math.max(
