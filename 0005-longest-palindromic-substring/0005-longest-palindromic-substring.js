dadd6795665074f9d57501e00e333b9998d87ae1
tkhwang <tkhwang@gmail.com>
2025-10-23 16:09:33 +0900

    Time: 608 ms (9.94%), Space: 91.9 MB (5.06%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -4,35 +4,29 @@
  */
 var longestPalindrome = function(s) {
     const N = s.length;
-    if (N <= 1) return s;
 
-    let start = 0;
-    let maxLength = 1;
-
-    const dp = Array(N).fill(0).map((_) => Array(N).fill(false));
+    const dp = Array(N).fill(null).map(() => Array(N).fill(false));
 
     for (let i = 0; i < N; i += 1) {
         dp[i][i] = true;
     }
 
-    for (let length = 2; length <= N; length += 1) {
-        for (let i = 0; i <= N - length; i += 1) {
-            // j - i + 1 = length
-            const j = i + length - 1;
-            if (s[i] === s[j]) {
-                if (length === 2) {
-                    dp[i][j] = true;
-                } else {
-                    dp[i][j] = dp[i+1][j-1]
-                }
+    let start = 0;
+    let max = 1;
 
-                if (dp[i][j] && length > maxLength) {
-                    start = i;
-                    maxLength = length;
+    for (let len = 2; len <= N; len += 1) {
+        for (let i = 0; i < N - len + 1; i += 1) {
+            const j = i + len - 1;
+            if (s[i] === s[j]) {
+                if (len === 2 || dp[i+1][j-1]) {
+                    dp[i][j] = true;
+                    if (max < len) {
+                        start = i;
+                        max = len;
+                    }
                 }
-            } 
+            }
         }
     }
-
-    return s.substring(start, start + maxLength);
+    return s.substring(start, start + max);
 };
