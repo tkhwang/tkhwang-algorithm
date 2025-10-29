d63f63ee999bf4683cf17486725965cfa615c277
tkhwang <tkhwang@gmail.com>
2025-10-29 12:01:44 +0900

    Time: 10 ms (73.33%), Space: 56.1 MB (13.33%) - LeetHub

---
+++ 2546-number-of-subarrays-with-gcd-equal-to-k/2546-number-of-subarrays-with-gcd-equal-to-k.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarrayGCD = function(nums, k) {
+    const N = nums.length;
+
+    const gcd = (a,b) => {
+        if (b === 0) return a;
+        return gcd(b, a % b);
+    }
+
+    let count = 0;
+    
+    for (let left = 0; left < N; left++) {
+        let currentGCD = 0;  
+        for (let right = left; right < N; right++) {
+            currentGCD = gcd(currentGCD, nums[right]);
+            
+            if (currentGCD === k) count++;
+            else if (currentGCD < k) break;
+        }
+    }
+    return count;
+};
