68208bb8cb458c6bab6596950f53c94263ddedb3
tkhwang <tkhwang@gmail.com>
2025-10-30 17:23:06 +0900

    Time: 1 ms (51.43%), Space: 57.4 MB (23.81%) - LeetHub

--- 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.js
+++ 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.js
@@ -6,17 +6,12 @@
 var minOperations = function(nums, k) {
     const N = nums.length;
 
-    const targets = Array(k).fill(null).map((v, i) => i + 1);
-
-    const set = new Set(targets);
-
+    const set = new Set();
     let count = 0;
     for (let i = N - 1; i >= 0; i -= 1) {
         count += 1;
 
-        const cur = nums[i];
-        if (set.has(cur)) set.delete(cur);
-        if (set.size === 0) return count;
+        if (nums[i] <= k) set.add(nums[i]);
+        if (set.size === k) return count;        
     }
-
 };
